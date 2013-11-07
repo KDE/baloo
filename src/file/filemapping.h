53ab743d7e6325c5d2e87e2a24319b5a0b8d8fe9
Vishesh Handa <me@vhanda.in>
2013-11-07 18:22:07 +0100

    Some hypothetical file APIs written about a month ago
    No point throwing them away without committing them

---
+++ src/file/filemapping.h
@@ -0,0 +1,47 @@
+/*
+ * <one line to give the library's name and an idea of what it does.>
+ * Copyright 2013  Vishesh Handa <me@vhanda.in>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef FILEMAPPING_H
+#define FILEMAPPING_H
+
+#include <QUrl>
+
+class FileMappingFetchJob;
+
+typedef int FileIdentifier;
+
+class FileMapping
+{
+public:
+    FileMapping(const QString& url);
+    FileMapping(FileIdentifier fid);
+
+    FileIdentifier fid();
+    QString url();
+
+    bool hasUrl();
+    bool hasFid();
+
+    FileMappingFetchJob* fetch();
+};
+
+#endif // FILEMAPPING_H
