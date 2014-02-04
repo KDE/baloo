ca02410ff4e9d6a2be6cffe44a296c94dc568149
Vishesh Handa <me@vhanda.in>
2014-02-04 16:59:53 +0100

    New IndexerConfig class
    Now Dolphin does not need to parse our config files on their own

---
+++ src/file/lib/indexerconfig.h
@@ -0,0 +1,46 @@
+/*
+ * <one line to give the library's name and an idea of what it does.>
+ * Copyright (C) 2014  Vishesh Handa <me@vhanda.in>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef BALOO_INDEXERCONFIG_H
+#define BALOO_INDEXERCONFIG_H
+
+#include <QObject>
+#include "file_export.h"
+
+namespace Baloo {
+
+class BALOO_FILE_EXPORT IndexerConfig
+{
+public:
+    IndexerConfig();
+    ~IndexerConfig();
+
+    bool balooEnabled() const;
+    bool fileIndexingEnabled() const;
+
+    bool shouldBeIndexed(const QString& path) const;
+
+private:
+    class Private;
+    Private* d;
+};
+}
+
+#endif // BALOO_INDEXERCONFIG_H
