56cdb9f4ddaf99f72d1eacc10935ffc1cf06eda4
Vishesh Handa <me@vhanda.in>
2014-03-12 22:35:57 +0100

    File: Add some documentation

--- src/file/lib/file.h
+++ src/file/lib/file.h
@@ -30,11 +30,24 @@ namespace Baloo {
 
 class FilePrivate;
 
+/**
+ * @short Provides acess to all File Metadata
+ *
+ * The File class acts as a temporary container for all the file metadata.
+ * It needs to be filled via the FileFetchJob, and any modifications made
+ * are not saved until the FileModifyJob is called.
+ */
 class BALOO_FILE_EXPORT File
 {
 public:
     File();
     File(const File& f);
+
+    /**
+     * Constructor
+     *
+     * \p url the local url of the file
+     */
     File(const QString& url);
     ~File();
 
@@ -42,9 +55,16 @@ public:
 
     static File fromId(const QByteArray& id);
 
+    /**
+     * The local url of the file
+     */
     QString url() const;
     void setUrl(const QString& url);
 
+    /**
+     * Represents a unique identifier for this file. This identifier
+     * is unique and will never change unlike the url of the file
+     */
     QByteArray id() const;
     void setId(const QByteArray& id);
 
@@ -55,9 +75,17 @@ public:
     KFileMetaData::PropertyMap properties() const;
     QVariant property(KFileMetaData::Property::Property property) const;
 
+    /**
+     * Set the rating for the file. This will not be saved until
+     * a FileModifyJob is called
+     */
     void setRating(int rating);
     int rating() const;
 
+    /**
+     * Add a tag to the list of tags. This will not be saved until
+     * a FileModifyJob is called.
+     */
     void addTag(const QString& tag);
     void setTags(const QStringList& tags);
     QStringList tags() const;
