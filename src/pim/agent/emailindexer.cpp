b17434cf8c3190a272b2b34058d9e3a1453fe120
Montel Laurent <montel@kde.org>
2014-03-21 09:55:52 +0100

    Initialize pointer. Fix potential crash

--- src/pim/agent/emailindexer.cpp
+++ src/pim/agent/emailindexer.cpp
@@ -28,7 +28,7 @@
 #include <QTextDocument>
 
 EmailIndexer::EmailIndexer(const QString& path, const QString& contactDbPath):
-    AbstractIndexer(), m_termGen( 0 )
+    AbstractIndexer(), m_doc( 0 ), m_termGen( 0 )
 {
     m_db = new Xapian::WritableDatabase(path.toStdString(), Xapian::DB_CREATE_OR_OPEN);
     m_contactDb = new Xapian::WritableDatabase(contactDbPath.toStdString(), Xapian::DB_CREATE_OR_OPEN);
