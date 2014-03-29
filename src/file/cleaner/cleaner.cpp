632abbd8a255bf01e334565e18c8b5d3c6ad0ebf
Vishesh Handa <me@vhanda.in>
2014-03-29 17:23:52 +0100

    Cleaner: Always remove it from both the databases

--- src/file/cleaner/cleaner.cpp
+++ src/file/cleaner/cleaner.cpp
@@ -29,6 +29,7 @@
 #include <QUrl>
 #include <QCoreApplication>
 #include <QSqlQuery>
+#include <QDebug>
 
 using namespace Baloo;
 
@@ -53,24 +54,27 @@ void Cleaner::start()
         int id = query.value(0).toInt();
         QString url = query.value(1).toString();
 
+        bool removeIt = false;
         if (!QFile::exists(url)) {
+            removeIt = true;
+        }
+
+        if (!config.shouldBeIndexed(url)) {
+            removeIt = true;
+        }
+
+        QString mimetype = KMimeType::findByUrl(QUrl::fromLocalFile(url))->name();
+        if (!config.shouldMimeTypeBeIndexed(mimetype)) {
+            removeIt = true;
+        }
+
+        if (removeIt) {
+            qDebug() << id << url;
             QSqlQuery q(sqlDb);
             q.prepare("delete from files where id = ?");
             q.addBindValue(id);
             q.exec();
             m_commitQueue->remove(id);
-            continue;
-        }
-
-        if (!config.shouldBeIndexed(url)) {
-            m_commitQueue->remove(id);
-            continue;
-        }
-
-        QString mimetype = KMimeType::findByUrl(QUrl::fromLocalFile(url))->name();
-        if (!config.shouldMimeTypeBeIndexed(mimetype)) {
-            m_commitQueue->remove(id);
-            continue;
         }
     }
 
