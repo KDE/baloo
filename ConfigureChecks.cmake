dc83e25d304b5339eb390981aa959906cd18a8b3
Patrick Spendrin <ps_ml@gmx.de>
2014-04-22 00:38:29 +0200

    exclude check for extended attributes for Windows

--- ConfigureChecks.cmake
+++ ConfigureChecks.cmake
@@ -4,6 +4,7 @@ include(CheckIncludeFiles)
 # least some file systems supported by the operating system. We currently
 # assume the functionality is in libc itself.
 
+if (NOT WIN32)
 # Linux and OS X.
 check_include_files("sys/types.h;sys/xattr.h" HAVE_SYS_XATTR_H)
 
@@ -12,4 +13,5 @@ check_include_files("sys/types.h;sys/extattr.h" HAVE_SYS_EXTATTR_H)
 
 if (NOT (HAVE_SYS_XATTR_H OR HAVE_SYS_EXTATTR_H))
     message(FATAL_ERROR "Baloo requires extended attributes support in the operating system, but no headers have been found.")
+endif ()
 endif ()
