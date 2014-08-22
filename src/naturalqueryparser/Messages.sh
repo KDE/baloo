43088a9749295b1225c46e49682b44eabacda9c5
Denis Steckelmacher <steckdenis@yahoo.fr>
2014-08-22 11:24:12 +0200

    Give a proper name to the .pot files generated by Messages.sh

--- src/naturalqueryparser/Messages.sh
+++ src/naturalqueryparser/Messages.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-$XGETTEXT `find . -name "*.cpp" | grep -v "/autotests/"` -o $podir/baloo_queryparser.pot
+$XGETTEXT `find . -name "*.cpp" | grep -v "/autotests/"` -o $podir/baloo_naturalqueryparser.pot
