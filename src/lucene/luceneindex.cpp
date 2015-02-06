95bc31bfec5c3f96773d8f8f3ca9fb5921b93fbf
Pinak Ahuja <pinak.ahuja@gmail.com>
2015-02-07 00:42:34 +0530

    LuceneIndex now only uses writer when required (while committing)
    Also add a writeOnly option

--- src/lucene/luceneindex.cpp
+++ src/lucene/luceneindex.cpp
@@ -4,22 +4,23 @@
 
 using namespace Baloo;
 
-LuceneIndex::LuceneIndex(const QString& path)
+LuceneIndex::LuceneIndex(const QString& path, bool writeOnly)
     : m_path(path)
+    , m_writeOnly(writeOnly)
 {
-    try {
-        m_indexWriter = Lucene::newLucene<Lucene::IndexWriter>(Lucene::FSDirectory::open(path.toStdWString()),
-            Lucene::newLucene<Lucene::StandardAnalyzer>(Lucene::LuceneVersion::LUCENE_CURRENT),
-            Lucene::IndexWriter::DEFAULT_MAX_FIELD_LENGTH);
+    if (m_writeOnly) {
+        m_indexWriter = openWriter();
     }
-    catch (Lucene::LuceneException& e) {
-         qWarning() << "Exception:" << e.getError().c_str();
+    else {
+        //creates index if it doesn't already exist
+        openWriter();
+
+        m_indexReader = Lucene::IndexReader::open(Lucene::FSDirectory::open( (m_path.toStdWString()) ), true);
     }
 }
 
 LuceneIndex::~LuceneIndex()
 {
-
 }
 
 
@@ -30,24 +31,37 @@ void LuceneIndex::addDocument(LuceneDocument& doc)
 
 void LuceneIndex::addDocument(Lucene::DocumentPtr doc)
 {
-    try {
-        m_indexWriter->addDocument(doc);
+    if (m_writeOnly) {
+        try {
+            m_indexWriter->addDocument(doc);
+        }
+        catch (Lucene::LuceneException &e) {
+            qWarning() << "Exception" << e.getError().c_str();
+        }
     }
-    catch (Lucene::LuceneException &e) {
-        qWarning() << "Exception" << e.getError().c_str();
+    else {
+        m_docsToAdd << doc;
     }
-    m_haveChanges = true;
 }
 
 void LuceneIndex::replaceDocument(const QString& url, const Lucene::DocumentPtr& doc)
 {
-    m_indexWriter->updateDocument(makeTerm(QStringLiteral("URL"), url), doc);
-    m_haveChanges = true;
+    if (m_writeOnly) {
+        m_indexWriter->updateDocument(makeTerm(QStringLiteral("URL"), url), doc);
+    }
+    else {
+        m_docsToReplace << qMakePair(url, doc);
+    }
 }
 
 void LuceneIndex::deleteDocument(const QString& url)
 {
-    m_indexWriter->deleteDocuments( makeTerm(QStringLiteral("URL"), url) );
+    if (m_writeOnly) {
+        m_indexWriter->deleteDocuments( makeTerm(QStringLiteral("URL"), url) );
+    }
+    else {
+        m_docsToDelete << url;
+    }
 }
 
 Lucene::TermPtr LuceneIndex::makeTerm(const QString& field, const QString& value)
@@ -58,21 +72,68 @@ Lucene::TermPtr LuceneIndex::makeTerm(const QString& field, const QString& value
 
 Lucene::IndexReaderPtr LuceneIndex::IndexReader()
 {
-    return m_indexWriter->getReader();
+    if (m_indexReader) {
+        reopenReader();
+        return m_indexReader;
+    }
+    else {
+        return m_indexWriter->getReader();
+    }
 }
 
-void LuceneIndex::commit(bool optimize)
+Lucene::IndexWriterPtr LuceneIndex::openWriter()
 {
-    try {
-        if (optimize){
-            m_indexWriter->optimize();
-        }
-        m_indexWriter->commit();
-    }
-    catch (Lucene::LuceneException &e) {
-        qWarning() << "Exception" << e.getError().c_str();
-    }
-    m_haveChanges = false;
+    Lucene::IndexWriterPtr writer = Lucene::newLucene<Lucene::IndexWriter>(Lucene::FSDirectory::open( (m_path.toStdWString()) ),
+                                        Lucene::newLucene<Lucene::StandardAnalyzer>(Lucene::LuceneVersion::LUCENE_CURRENT),
+                                        Lucene::IndexWriter::MaxFieldLengthLIMITED);
+    return writer;
 }
 
+bool LuceneIndex::haveChanges()
+{
+    return !m_docsToAdd.isEmpty() || !m_docsToReplace.isEmpty() || !m_docsToDelete.isEmpty();
+}
 
+void LuceneIndex::commit()
+{
+    if (m_writeOnly) {
+        try {
+            m_indexWriter->commit();
+        }
+        catch (Lucene::LuceneException &e) {
+            qWarning() << "Exception" << e.getError().c_str();
+        }
+    }
+
+    if (!haveChanges()) {
+        return;
+    }
+
+    Lucene::IndexWriterPtr writer = openWriter();
+    Q_FOREACH (const Lucene::DocumentPtr& doc, m_docsToAdd) {
+        writer->addDocument(doc);
+    }
+
+    Q_FOREACH (const urlDocPair& pair, m_docsToReplace) {
+        writer->updateDocument( makeTerm(QStringLiteral("URL"), pair.first) , pair.second );
+    }
+
+    Q_FOREACH (const QString url, m_docsToDelete) {
+        writer->deleteDocuments( makeTerm(QStringLiteral("URL"), url) );
+    }
+
+    writer->commit();
+    reopenReader();
+    m_docsToAdd.clear();
+    m_docsToDelete.clear();
+    m_docsToReplace.clear();
+}
+
+void LuceneIndex::reopenReader()
+{
+    Lucene::IndexReaderPtr newReader = m_indexReader->reopen();
+    if (newReader != m_indexReader) {
+        m_indexReader->close();
+        m_indexReader = newReader;
+    }
+}
--- src/lucene/luceneindex.h
+++ src/lucene/luceneindex.h
@@ -4,28 +4,39 @@
 #include <lucene++/LuceneHeaders.h>
 #include "lucenedocument.h"
 #include "lucene_export.h"
+#include <QVector>
+#include <QPair>
+#include <QStringList>
 
 namespace Baloo {
 class BALOO_LUCENE_EXPORT LuceneIndex {
 public:
-    LuceneIndex(const QString& path);
+    LuceneIndex(const QString& path, bool writeOnly = false);
     ~LuceneIndex();
 
     void addDocument(LuceneDocument& doc);
     void addDocument(Lucene::DocumentPtr doc);
     void replaceDocument(const QString& url, const Lucene::DocumentPtr& doc);
     void deleteDocument(const QString& url);
-    void commit(bool optimize = 0);
-    bool haveChanges() { return m_haveChanges; }
+    void commit();
+    bool haveChanges();
     Lucene::IndexWriterPtr indexWriter() { return m_indexWriter; }
     QString path() { return m_path; }
     Lucene::IndexReaderPtr IndexReader();
 
 private:
     Lucene::TermPtr makeTerm(const QString& field, const QString& value);
+    Lucene::IndexWriterPtr openWriter();
+    void reopenReader();
+
     Lucene::IndexWriterPtr m_indexWriter;
-    bool m_haveChanges = false;
+    Lucene::IndexReaderPtr m_indexReader;
     QString m_path;
+    bool m_writeOnly;
+    QVector<Lucene::DocumentPtr> m_docsToAdd;
+    typedef QPair<QString, Lucene::DocumentPtr> urlDocPair;
+    QVector<urlDocPair> m_docsToReplace;
+    QStringList m_docsToDelete;
 };
 }
 
