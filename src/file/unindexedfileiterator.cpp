ba85fb57c47ac68512626ee0d017d61427641d0a
Vishesh Handa <me@vhanda.in>
2015-04-09 16:00:25 +0200

    Add an UnIndexedFileIterator
    This provides an iterator which can be used to only iterate over all the
    files under a certain path which need to be reindexed. This internally
    used the FilteredDirIterator which is quite efficient since it doesn't
    check the entire file path each time and only checks the changes.
    
    We can use this UnIndexedFileIterator in the BasicIQ later and
    substantially improve performance.

---
+++ src/file/unindexedfileiterator.cpp
@@ -0,0 +1,91 @@
+/*
+ * This file is part of the KDE Baloo project.
+ * Copyright (C) 2015  Vishesh Handa <vhanda@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include "unindexedfileiterator.h"
+#include "fileindexerconfig.h"
+#include "idutils.h"
+#include "transaction.h"
+
+#include <QFileInfo>
+#include <QDateTime>
+
+using namespace Baloo;
+
+UnIndexedFileIterator::UnIndexedFileIterator(FileIndexerConfig* config, Transaction* transaction, const QString& folder)
+    : m_config(config)
+    , m_transaction(transaction)
+    , m_iter(config, folder, FilteredDirIterator::FilesAndDirs)
+{
+}
+
+UnIndexedFileIterator::~UnIndexedFileIterator()
+{
+}
+
+QString UnIndexedFileIterator::filePath() const
+{
+    return m_iter.filePath();
+}
+
+QString UnIndexedFileIterator::next()
+{
+    const QString filePath = m_iter.next();
+
+    // This mimetype may not be completely accurate, but that's okay. This is
+    // just the initial phase of indexing. The second phase can try to find
+    // a more accurate mimetype.
+    QString mimetype = m_mimeDb.mimeTypeForFile(filePath, QMimeDatabase::MatchExtension).name();
+
+    if (!shouldIndex(filePath, mimetype)) {
+        return next();
+    }
+
+    return filePath;
+}
+
+bool UnIndexedFileIterator::shouldIndex(const QString& filePath, const QString& mimetype)
+{
+    bool shouldIndexType = m_config->shouldMimeTypeBeIndexed(mimetype);
+    if (!shouldIndexType)
+        return false;
+
+    QFileInfo fileInfo(filePath);
+    if (!fileInfo.exists())
+        return false;
+
+    quint64 fileId = filePathToId(QFile::encodeName(filePath));
+    Q_ASSERT_X(fileId, "UnIndexedFileIterator::shouldIndex", "file id is 0");
+    if (!fileId) {
+        return true;
+    }
+
+    // A folders mtime is updated when a new file is added / removed / renamed
+    // we don't really need to reindex a folder when that happens
+    // In fact, we never need to reindex a folder
+    if (mimetype == QLatin1String("inode/directory"))
+        return false;
+
+    quint64 mTime = m_transaction->documentMTime(fileId);
+    if (mTime != fileInfo.lastModified().toTime_t()) {
+        return true;
+    }
+
+    return false;
+}
---
+++ src/file/unindexedfileiterator.h
@@ -0,0 +1,61 @@
+/*
+ * This file is part of the KDE Baloo project.
+ * Copyright (C) 2015  Vishesh Handa <vhanda@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef BALOO_UNINDEXEDFILEITERATOR_H
+#define BALOO_UNINDEXEDFILEITERATOR_H
+
+#include "filtereddiriterator.h"
+
+#include <QMimeDatabase>
+
+namespace Baloo {
+
+class Transaction;
+
+/**
+ * Iterate over all the files (and directories) under a specific directory which require
+ * indexing. This checks the following -
+ * - Config include / exclude path
+ * - Config filters
+ * - Config mimetype filters
+ * - Datbase for mtime differences
+ */
+class UnIndexedFileIterator
+{
+public:
+    UnIndexedFileIterator(FileIndexerConfig* config, Transaction* transaction, const QString& folder);
+    ~UnIndexedFileIterator();
+
+    QString next();
+    QString filePath() const;
+
+private:
+    bool shouldIndex(const QString& filePath, const QString& mimetype);
+
+    FileIndexerConfig* m_config;
+    Transaction* m_transaction;
+    FilteredDirIterator m_iter;
+
+    QMimeDatabase m_mimeDb;
+};
+
+}
+
+#endif // BALOO_UNINDEXEDFILEITERATOR_H
