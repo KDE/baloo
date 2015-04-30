3885e68c267f430cd56776033f1ef67faf723d3b
Vishesh Handa <me@vhanda.in>
2015-04-30 15:46:06 +0200

    Add fixed and varint encoding functions - Taken from LevelDB

---
+++ src/codecs/coding.cpp
@@ -0,0 +1,242 @@
+/*
+ * This file is part of the KDE Baloo project.
+ * Copyright (C) 2015  Vishesh Handa <vhanda@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+/*
+    Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are
+    met:
+
+    * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+    copyright notice, this list of conditions and the following disclaimer
+    in the documentation and/or other materials provided with the
+    distribution.
+    * Neither the name of Google Inc. nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+    A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+    OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "coding.h"
+
+namespace Baloo {
+
+void EncodeFixed32(char* buf, uint32_t value) {
+    if (1 /*port::kLittleEndian*/) {
+        memcpy(buf, &value, sizeof(value));
+    } else {
+        buf[0] = value & 0xff;
+        buf[1] = (value >> 8) & 0xff;
+        buf[2] = (value >> 16) & 0xff;
+        buf[3] = (value >> 24) & 0xff;
+    }
+}
+
+void EncodeFixed64(char* buf, uint64_t value) {
+    if (1 /*port::kLittleEndian*/) {
+        memcpy(buf, &value, sizeof(value));
+    } else {
+        buf[0] = value & 0xff;
+        buf[1] = (value >> 8) & 0xff;
+        buf[2] = (value >> 16) & 0xff;
+        buf[3] = (value >> 24) & 0xff;
+        buf[4] = (value >> 32) & 0xff;
+        buf[5] = (value >> 40) & 0xff;
+        buf[6] = (value >> 48) & 0xff;
+        buf[7] = (value >> 56) & 0xff;
+    }
+}
+
+void putFixed32(QByteArray* dst, quint32 value) {
+    char buf[sizeof(value)];
+    EncodeFixed32(buf, value);
+    dst->append(buf, sizeof(buf));
+}
+
+void putFixed64(QByteArray* dst, quint64 value) {
+    char buf[sizeof(value)];
+    EncodeFixed64(buf, value);
+    dst->append(buf, sizeof(buf));
+}
+
+char* EncodeVarint32(char* dst, uint32_t v) {
+    // Operate on characters as unsigneds
+    unsigned char* ptr = reinterpret_cast<unsigned char*>(dst);
+    static const int B = 128;
+    if (v < (1<<7)) {
+        *(ptr++) = v;
+    } else if (v < (1<<14)) {
+        *(ptr++) = v | B;
+        *(ptr++) = v>>7;
+    } else if (v < (1<<21)) {
+        *(ptr++) = v | B;
+        *(ptr++) = (v>>7) | B;
+        *(ptr++) = v>>14;
+    } else if (v < (1<<28)) {
+        *(ptr++) = v | B;
+        *(ptr++) = (v>>7) | B;
+        *(ptr++) = (v>>14) | B;
+        *(ptr++) = v>>21;
+    } else {
+        *(ptr++) = v | B;
+        *(ptr++) = (v>>7) | B;
+        *(ptr++) = (v>>14) | B;
+        *(ptr++) = (v>>21) | B;
+        *(ptr++) = v>>28;
+    }
+    return reinterpret_cast<char*>(ptr);
+}
+
+void putVarint32(QByteArray* dst, quint32 v) {
+    char buf[5];
+    char* ptr = EncodeVarint32(buf, v);
+    dst->append(buf, ptr - buf);
+}
+
+char* EncodeVarint64(char* dst, uint64_t v) {
+    static const int B = 128;
+    unsigned char* ptr = reinterpret_cast<unsigned char*>(dst);
+    while (v >= B) {
+        *(ptr++) = (v & (B-1)) | B;
+        v >>= 7;
+    }
+    *(ptr++) = static_cast<unsigned char>(v);
+    return reinterpret_cast<char*>(ptr);
+}
+
+void putVarint64(QByteArray* dst, quint64 v) {
+    char buf[10];
+    char* ptr = EncodeVarint64(buf, v);
+    dst->append(buf, ptr - buf);
+}
+
+/*
+void PutLengthPrefixedSlice(std::string* dst, const Slice& value) {
+    PutVarint32(dst, value.size());
+    dst->append(value.data(), value.size());
+}
+
+int VarintLength(uint64_t v) {
+    int len = 1;
+    while (v >= 128) {
+        v >>= 7;
+        len++;
+    }
+    return len;
+}
+
+const char* GetVarint32PtrFallback(const char* p,
+                                   const char* limit,
+                                   uint32_t* value) {
+    uint32_t result = 0;
+    for (uint32_t shift = 0; shift <= 28 && p < limit; shift += 7) {
+        uint32_t byte = *(reinterpret_cast<const unsigned char*>(p));
+        p++;
+        if (byte & 128) {
+            // More bytes are present
+            result |= ((byte & 127) << shift);
+        } else {
+            result |= (byte << shift);
+            *value = result;
+            return reinterpret_cast<const char*>(p);
+        }
+    }
+    return NULL;
+}
+
+bool GetVarint32(Slice* input, uint32_t* value) {
+    const char* p = input->data();
+    const char* limit = p + input->size();
+    const char* q = GetVarint32Ptr(p, limit, value);
+    if (q == NULL) {
+        return false;
+    } else {
+        *input = Slice(q, limit - q);
+        return true;
+    }
+}
+
+const char* GetVarint64Ptr(const char* p, const char* limit, uint64_t* value) {
+    uint64_t result = 0;
+    for (uint32_t shift = 0; shift <= 63 && p < limit; shift += 7) {
+        uint64_t byte = *(reinterpret_cast<const unsigned char*>(p));
+        p++;
+        if (byte & 128) {
+            // More bytes are present
+            result |= ((byte & 127) << shift);
+        } else {
+            result |= (byte << shift);
+            *value = result;
+            return reinterpret_cast<const char*>(p);
+        }
+    }
+    return NULL;
+}
+
+bool GetVarint64(Slice* input, uint64_t* value) {
+    const char* p = input->data();
+    const char* limit = p + input->size();
+    const char* q = GetVarint64Ptr(p, limit, value);
+    if (q == NULL) {
+        return false;
+    } else {
+        *input = Slice(q, limit - q);
+        return true;
+    }
+}
+
+const char* GetLengthPrefixedSlice(const char* p, const char* limit,
+                                   Slice* result) {
+    uint32_t len;
+    p = GetVarint32Ptr(p, limit, &len);
+    if (p == NULL) return NULL;
+    if (p + len > limit) return NULL;
+    *result = Slice(p, len);
+    return p + len;
+}
+
+bool GetLengthPrefixedSlice(Slice* input, Slice* result) {
+    uint32_t len;
+    if (GetVarint32(input, &len) &&
+            input->size() >= len) {
+        *result = Slice(input->data(), len);
+        input->remove_prefix(len);
+        return true;
+    } else {
+        return false;
+    }
+}
+*/
+
+}
---
+++ src/codecs/coding.h
@@ -0,0 +1,148 @@
+/*
+ * This file is part of the KDE Baloo project.
+ * Copyright (C) 2015  Vishesh Handa <vhanda@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+/*
+    Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are
+    met:
+
+    * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+    copyright notice, this list of conditions and the following disclaimer
+    in the documentation and/or other materials provided with the
+    distribution.
+    * Neither the name of Google Inc. nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+    A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+    OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef BALOO_STORAGE_LEVELDB_UTIL_CODING_H
+#define BALOO_STORAGE_LEVELDB_UTIL_CODING_H
+
+#include <QByteArray>
+
+#include <stdint.h>
+#include <string.h>
+#include <string>
+
+namespace Baloo {
+
+// Standard Put... routines append to a string
+void putFixed32(QByteArray* dst, quint32 value);
+void putFixed64(QByteArray* dst, quint64 value);
+void putVarint32(QByteArray* dst, quint32 value);
+void putVarint64(QByteArray* dst, quint64 value);
+
+/*
+void PutLengthPrefixedSlice(std::string* dst, const Slice& value);
+
+// Standard Get... routines parse a value from the beginning of a Slice
+// and advance the slice past the parsed value.
+bool GetVarint32(Slice* input, uint32_t* value);
+bool GetVarint64(Slice* input, uint64_t* value);
+bool GetLengthPrefixedSlice(Slice* input, Slice* result);
+*/
+
+// Pointer-based variants of GetVarint...  These either store a value
+// in *v and return a pointer just past the parsed value, or return
+// NULL on error.  These routines only look at bytes in the range
+// [p..limit-1]
+extern const char* GetVarint32Ptr(const char* p,const char* limit, uint32_t* v);
+extern const char* GetVarint64Ptr(const char* p,const char* limit, uint64_t* v);
+
+// Returns the length of the varint32 or varint64 encoding of "v"
+extern int VarintLength(uint64_t v);
+
+// Lower-level versions of Put... that write directly into a character buffer
+// REQUIRES: dst has enough space for the value being written
+extern void EncodeFixed32(char* dst, uint32_t value);
+extern void EncodeFixed64(char* dst, uint64_t value);
+
+// Lower-level versions of Put... that write directly into a character buffer
+// and return a pointer just past the last byte written.
+// REQUIRES: dst has enough space for the value being written
+extern char* EncodeVarint32(char* dst, uint32_t value);
+extern char* EncodeVarint64(char* dst, uint64_t value);
+
+// Lower-level versions of Get... that read directly from a character buffer
+// without any bounds checking.
+
+inline uint32_t decodeFixed32(const char* ptr) {
+    if (1 /*port::kLittleEndian*/) {
+        // Load the raw bytes
+        uint32_t result;
+        memcpy(&result, ptr, sizeof(result));  // gcc optimizes this to a plain load
+        return result;
+    } else {
+        return ((static_cast<uint32_t>(static_cast<unsigned char>(ptr[0])))
+                | (static_cast<uint32_t>(static_cast<unsigned char>(ptr[1])) << 8)
+                | (static_cast<uint32_t>(static_cast<unsigned char>(ptr[2])) << 16)
+                | (static_cast<uint32_t>(static_cast<unsigned char>(ptr[3])) << 24));
+    }
+}
+
+inline uint64_t decodeFixed64(const char* ptr) {
+    if (1 /*port::kLittleEndian*/) {
+        // Load the raw bytes
+        uint64_t result;
+        memcpy(&result, ptr, sizeof(result));  // gcc optimizes this to a plain load
+        return result;
+    } else {
+        uint64_t lo = decodeFixed32(ptr);
+        uint64_t hi = decodeFixed32(ptr + 4);
+        return (hi << 32) | lo;
+    }
+}
+
+// Internal routine for use by fallback path of GetVarint32Ptr
+extern const char* GetVarint32PtrFallback(const char* p,
+        const char* limit,
+        uint32_t* value);
+inline const char* GetVarint32Ptr(const char* p,
+                                  const char* limit,
+                                  uint32_t* value) {
+    if (p < limit) {
+        uint32_t result = *(reinterpret_cast<const unsigned char*>(p));
+        if ((result & 128) == 0) {
+            *value = result;
+            return p + 1;
+        }
+    }
+    return GetVarint32PtrFallback(p, limit, value);
+}
+
+}
+
+#endif
