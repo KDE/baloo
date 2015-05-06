3ed942b2c84696cd658ce5eadd2b5b9833997bd5
Vishesh Handa <me@vhanda.in>
2015-05-06 15:35:52 +0200

    LMDB: Use MDB_NOMEMINIT
    We are not dealing with sensitive information. We do not need the extra
    performance cost to initialize the memory to 0

--- src/engine/database.cpp
+++ src/engine/database.cpp
@@ -69,7 +69,7 @@ bool Database::open(OpenMode mode)
 
     // The directory needs to be created before opening the environment
     QByteArray arr = QFile::encodeName(m_path) + "/index";
-    mdb_env_open(m_env, arr.constData(), MDB_NOSUBDIR, 0664);
+    mdb_env_open(m_env, arr.constData(), MDB_NOSUBDIR | MDB_NOMEMINIT, 0664);
     int rc = mdb_reader_check(m_env, 0);
     Q_ASSERT_X(rc == 0, "Database::open reader_check", mdb_strerror(rc));
 
