e2f2c3c60abdc03bf45f564ddcb1436187147485
Vishesh Handa <me@vhanda.in>
2015-05-11 11:06:34 +0200

    Coding: More style fixes
    * Use quint32 and quint64 instead of uint32_t and uint64_T
    * Remove all the endian codes, we're not endian safe. We do not care
      about it.
    * Fix brackets after functions

--- src/codecs/coding.h
+++ src/codecs/coding.h
@@ -53,10 +53,6 @@
 
 #include <QByteArray>
 
-#include <stdint.h>
-#include <string.h>
-#include <string>
-
 namespace Baloo {
 
 // Standard Put... routines append to a string
@@ -76,59 +72,48 @@ bool getVarint64(QByteArray* input, quint64* value);
 // in *v and return a pointer just past the parsed value, or return
 // NULL on error.  These routines only look at bytes in the range
 // [p..limit-1]
-extern const char* GetVarint32Ptr(const char* p,const char* limit, quint32* v);
-extern const char* getVarint64Ptr(const char* p,const char* limit, quint64* v);
+extern const char* getVarint32Ptr(const char* p, const char* limit, quint32* v);
+extern const char* getVarint64Ptr(const char* p, const char* limit, quint64* v);
 
 // Returns the length of the varint32 or varint64 encoding of "v"
-extern int varintLength(uint64_t v);
+extern int varintLength(quint64 v);
 
 // Lower-level versions of Put... that write directly into a character buffer
 // REQUIRES: dst has enough space for the value being written
-extern void encodeFixed32(char* dst, uint32_t value);
-extern void encodeFixed64(char* dst, uint64_t value);
+extern void encodeFixed32(char* dst, quint32 value);
+extern void encodeFixed64(char* dst, quint64 value);
 
 // Lower-level versions of Put... that write directly into a character buffer
 // and return a pointer just past the last byte written.
 // REQUIRES: dst has enough space for the value being written
-extern char* encodeVarint32(char* dst, uint32_t value);
-extern char* encodeVarint64(char* dst, uint64_t value);
+extern char* encodeVarint32(char* dst, quint32 value);
+extern char* encodeVarint64(char* dst, quint64 value);
 
 // Lower-level versions of Get... that read directly from a character buffer
 // without any bounds checking.
 
-inline uint32_t decodeFixed32(const char* ptr) {
-    if (1 /*port::kLittleEndian*/) {
-        // Load the raw bytes
-        uint32_t result;
-        memcpy(&result, ptr, sizeof(result));  // gcc optimizes this to a plain load
-        return result;
-    } else {
-        return ((static_cast<uint32_t>(static_cast<unsigned char>(ptr[0])))
-                | (static_cast<uint32_t>(static_cast<unsigned char>(ptr[1])) << 8)
-                | (static_cast<uint32_t>(static_cast<unsigned char>(ptr[2])) << 16)
-                | (static_cast<uint32_t>(static_cast<unsigned char>(ptr[3])) << 24));
-    }
+inline quint32 decodeFixed32(const char* ptr)
+{
+    // Load the raw bytes
+    quint32 result;
+    memcpy(&result, ptr, sizeof(result));  // gcc optimizes this to a plain load
+    return result;
 }
 
-inline uint64_t decodeFixed64(const char* ptr) {
-    if (1 /*port::kLittleEndian*/) {
-        // Load the raw bytes
-        uint64_t result;
-        memcpy(&result, ptr, sizeof(result));  // gcc optimizes this to a plain load
-        return result;
-    } else {
-        uint64_t lo = decodeFixed32(ptr);
-        uint64_t hi = decodeFixed32(ptr + 4);
-        return (hi << 32) | lo;
-    }
+inline quint64 decodeFixed64(const char* ptr)
+{
+    // Load the raw bytes
+    quint64 result;
+    memcpy(&result, ptr, sizeof(result));  // gcc optimizes this to a plain load
+    return result;
 }
 
 // Internal routine for use by fallback path of GetVarint32Ptr
-extern char* getVarint32PtrFallback(char* p, char* limit, uint32_t* value);
-inline char* getVarint32Ptr(char* p, char* limit, uint32_t* value)
+extern char* getVarint32PtrFallback(char* p, char* limit, quint32* value);
+inline char* getVarint32Ptr(char* p, char* limit, quint32* value)
 {
     if (p < limit) {
-        uint32_t result = *(reinterpret_cast<const unsigned char*>(p));
+        quint32 result = *(reinterpret_cast<const unsigned char*>(p));
         if ((result & 128) == 0) {
             *value = result;
             return p + 1;
