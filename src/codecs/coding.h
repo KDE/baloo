f203d6034ae07d0fc2de681859f753bc8e8da201
Vishesh Handa <me@vhanda.in>
2015-05-11 10:41:17 +0200

    PositionCodec: Use varints for the positions
    The number of positions we have is often quite small, this way we save
    on the number of bytes used. There doesn't seem to be a performance
    difference with this vs raw encoding.

--- src/codecs/coding.h
+++ src/codecs/coding.h
@@ -65,25 +65,22 @@ void putFixed64(QByteArray* dst, quint64 value);
 void putVarint32(QByteArray* dst, quint32 value);
 void putVarint64(QByteArray* dst, quint64 value);
 
-/*
-void PutLengthPrefixedSlice(std::string* dst, const Slice& value);
-
 // Standard Get... routines parse a value from the beginning of a Slice
 // and advance the slice past the parsed value.
-bool GetVarint32(Slice* input, uint32_t* value);
-bool GetVarint64(Slice* input, uint64_t* value);
-bool GetLengthPrefixedSlice(Slice* input, Slice* result);
-*/
+bool getFixed32(QByteArray* input, quint32* value);
+bool getFixed64(QByteArray* input, quint64* value);
+bool getVarint32(QByteArray* input, quint32* value);
+bool getVarint64(QByteArray* input, quint64* value);
 
 // Pointer-based variants of GetVarint...  These either store a value
 // in *v and return a pointer just past the parsed value, or return
 // NULL on error.  These routines only look at bytes in the range
 // [p..limit-1]
-extern const char* GetVarint32Ptr(const char* p,const char* limit, uint32_t* v);
-extern const char* GetVarint64Ptr(const char* p,const char* limit, uint64_t* v);
+extern const char* GetVarint32Ptr(const char* p,const char* limit, quint32* v);
+extern const char* GetVarint64Ptr(const char* p,const char* limit, quint64* v);
 
 // Returns the length of the varint32 or varint64 encoding of "v"
-extern int VarintLength(uint64_t v);
+extern int varintLength(uint64_t v);
 
 // Lower-level versions of Put... that write directly into a character buffer
 // REQUIRES: dst has enough space for the value being written
@@ -127,12 +124,9 @@ inline uint64_t decodeFixed64(const char* ptr) {
 }
 
 // Internal routine for use by fallback path of GetVarint32Ptr
-extern const char* GetVarint32PtrFallback(const char* p,
-        const char* limit,
-        uint32_t* value);
-inline const char* GetVarint32Ptr(const char* p,
-                                  const char* limit,
-                                  uint32_t* value) {
+extern char* getVarint32PtrFallback(char* p, char* limit, uint32_t* value);
+inline char* getVarint32Ptr(char* p, char* limit, uint32_t* value)
+{
     if (p < limit) {
         uint32_t result = *(reinterpret_cast<const unsigned char*>(p));
         if ((result & 128) == 0) {
@@ -140,7 +134,7 @@ inline const char* GetVarint32Ptr(const char* p,
             return p + 1;
         }
     }
-    return GetVarint32PtrFallback(p, limit, value);
+    return getVarint32PtrFallback(p, limit, value);
 }
 
 }
