44992dec867376380c9f3a2fdcbf60240638b728
Vishesh Handa <me@vhanda.in>
2015-05-21 21:18:08 +0200

    pkg-config: prepend the cmake install prefix
    Not sure why the prefix parameter on the top doesn't seem to get
    automatically applied.

--- src/lib/Baloo.pc.cmake
+++ src/lib/Baloo.pc.cmake
@@ -8,5 +8,5 @@ Description: Baloo is a file indexing and searching framework for Linux
 URL: http://www.kde.org
 Requires: Qt5Core
 Version: ${BALOO_VERSION_STRING}
-Libs: -L${LIB_INSTALL_DIR} -lKF5Baloo
-Cflags: -I${KF5_INCLUDE_INSTALL_DIR}/Baloo
+Libs: -L${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR} -lKF5Baloo
+Cflags: -I${CMAKE_INSTALL_PREFIX}/${KF5_INCLUDE_INSTALL_DIR}/Baloo
