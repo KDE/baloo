2f43f0e7dbf7d2eef4256d816395e3bf9f39f23c
Vishesh Handa <me@vhanda.in>
2015-05-21 17:31:19 +0200

    Add support for pkg-config with the name Baloo

---
+++ src/lib/Baloo.pc.cmake
@@ -0,0 +1,12 @@
+prefix=${CMAKE_INSTALL_PREFIX}
+exec_prefix=${BIN_INSTALL_DIR}
+libdir=${LIB_INSTALL_DIR}
+includedir=${KF5_INCLUDE_INSTALL_DIR}
+
+Name: Baloo
+Description: Baloo is a file indexing and searching framework for Linux
+URL: http://www.kde.org
+Requires: Qt5Core
+Version: ${BALOO_VERSION_STRING}
+Libs: -L${LIB_INSTALL_DIR} -lKF5Baloo
+Cflags: -I${KF5_INCLUDE_INSTALL_DIR}/Baloo
