d97d26f1aeead58037cff671415e0174e3beae61
Vishesh Handa <me@vhanda.in>
2015-06-08 20:36:11 +0200

    Split up the FileIQ into 3 classes
    * FileContentIndexer - A QRunnable which just sends the list of ids
      along for indexing.
    
    * FileContentIndexerProvider - A light wrapper class which is just
      supposed to fetch the ids requried for indexing.
    
    * ExtractorProcess - Communicating with the extractor process and
      handling all the things that need to be done.
    
    All of these classes are very bare-bones. This commit is mostly for
    seeing the interface of the classes and if this new design will actually
    work out.

---
+++ src/file/filecontentindexerprovider.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2015  Vishesh Handa <vhanda@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef BALOO_FILECONTENTINDEXERPROVIDER_H
+#define BALOO_FILECONTENTINDEXERPROVIDER_H
+
+#include <QVector>
+
+namespace Baloo {
+
+class Database;
+
+class FileContentIndexerProvider
+{
+public:
+    FileContentIndexerProvider(Database* db);
+
+    uint size();
+    QVector<quint64> fetch(uint size);
+
+private:
+    Database* m_db;
+};
+}
+
+#endif // BALOO_FILECONTENTINDEXERPROVIDER_H
