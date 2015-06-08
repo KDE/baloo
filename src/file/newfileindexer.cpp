9a4d46ba025ff7f1b4d4b9a6bf2c6a241c3f3420
Vishesh Handa <me@vhanda.in>
2015-06-08 19:03:26 +0200

    Add a NewFileIndexer
    This is the first in the many number of indexers which are meant to
    replace the entire Basic + FileIQ archiecture of baloo_file.
    
    It would be nice to have unit tests, but that would require mocking out
    stuff.

---
+++ src/file/newfileindexer.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2015  Vishesh Handa <vhanda@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include "newfileindexer.h"
+#include "basicindexingjob.h"
+#include "fileindexerconfig.h"
+
+#include "database.h"
+#include "transaction.h"
+
+#include <QMimeDatabase>
+
+using namespace Baloo;
+
+NewFileIndexer::NewFileIndexer(Database* db, FileIndexerConfig* config, const QStringList& newFiles)
+    : m_db(db)
+    , m_config(config)
+    , m_files(newFiles)
+{
+}
+
+void NewFileIndexer::run()
+{
+    QMimeDatabase mimeDb;
+
+    Transaction tr(m_db, Transaction::ReadWrite);
+
+    for (const QString& filePath : m_files) {
+        Q_ASSERT(!filePath.endsWith('/'));
+
+        QString fileName = filePath.mid(filePath.lastIndexOf('/'));
+        if (!m_config->shouldFileBeIndexed(fileName)) {
+            continue;
+        }
+
+        QString mimetype = mimeDb.mimeTypeForFile(filePath, QMimeDatabase::MatchExtension).name();
+        if (!m_config->shouldMimeTypeBeIndexed(mimetype)) {
+            continue;
+        }
+
+        BasicIndexingJob job(filePath, mimetype, false);
+        if (!job.index()) {
+            continue;
+        }
+
+        tr.addDocument(job.document());
+    }
+
+    tr.commit();
+    Q_EMIT done();
+}
---
+++ src/file/newfileindexer.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2015  Vishesh Handa <vhanda@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef BALOO_NEWFILEINDEXER_H
+#define BALOO_NEWFILEINDEXER_H
+
+#include <QRunnable>
+#include <QStringList>
+#include <QObject>
+
+namespace Baloo {
+
+class Database;
+class FileIndexerConfig;
+
+/**
+ * Does not check the folder path or the mtime of the file
+ */
+class NewFileIndexer : public QObject, public QRunnable
+{
+    Q_OBJECT
+public:
+    NewFileIndexer(Database* db, FileIndexerConfig* config, const QStringList& newFiles);
+
+    void run() Q_DECL_OVERRIDE;
+
+Q_SIGNALS:
+    void done();
+
+private:
+    Database* m_db;
+    FileIndexerConfig* m_config;
+    QStringList m_files;
+};
+
+}
+
+#endif // BALOO_NEWFILEINDEXER_H
