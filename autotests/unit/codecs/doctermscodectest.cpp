e1fe14f5ecc6bf7e78ba277223eb2f3340611af4
Vishesh Handa <me@vhanda.in>
2015-06-10 23:57:05 +0200

    Add a DocTermsCodec test

---
+++ autotests/unit/codecs/doctermscodectest.cpp
@@ -0,0 +1,47 @@
+/*
+   This file is part of the KDE Baloo project.
+ * Copyright (C) 2015  Vishesh Handa <vhanda@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include "doctermscodec.h"
+
+#include <QObject>
+#include <QTemporaryDir>
+#include <QTest>
+
+using namespace Baloo;
+
+class DocTermsCodecTest : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void test() {
+        DocTermsCodec codec;
+
+        QVector<QByteArray> vec = {"ab", "abc", "dar", "darwin"};
+        QByteArray arr = codec.encode(vec);
+        QVERIFY(!arr.isEmpty());
+
+        QVector<QByteArray> vec2 = codec.decode(arr);
+        QCOMPARE(vec2, vec);
+    }
+};
+
+QTEST_MAIN(DocTermsCodecTest)
+
+#include "doctermscodectest.moc"
