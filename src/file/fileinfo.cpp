fe5af3596f40315a3ac34874aa2d26184c5ea5e1
Vishesh Handa <me@vhanda.in>
2015-09-06 07:03:37 +0200

    Add a FileInfo class
    This FileInfo class is meant to be a replacement of QFileInfo. Our
    replacement just directly calls 'stat' internally. It takes a QByteArray
    instead of QString and therefore we can avoid the utf16 -> utf8
    conversion.
    
    It should also be a little bit faster as we are avoiding all the extra
    logic inside QFileInfo which we do not care about.

---
+++ src/file/fileinfo.cpp
@@ -0,0 +1,40 @@
+/*
+ * This file is part of the KDE Baloo Project
+ * Copyright (C) 2015  Vishesh Handa <vhanda@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include "fileinfo.h"
+
+using namespace Baloo;
+
+FileInfo::FileInfo(const QByteArray& filePath)
+    : m_filePath(filePath)
+    , m_exists(true)
+{
+    Q_ASSERT(!filePath.endsWith('/'));
+    if (QT_LSTAT(filePath.data(), &m_statBuf) != 0) {
+        m_exists = false;
+        memset(&m_statBuf, 0, sizeof(QT_STATBUF));
+    }
+}
+
+bool FileInfo::isHidden() const
+{
+    int pos = m_filePath.lastIndexOf('/');
+    return m_filePath[pos + 1] == '.';
+}
