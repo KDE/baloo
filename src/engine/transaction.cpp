a3f83b980cd0f0f51397c5a2637f9b8795722327
Pinak Ahuja <pinak.ahuja@gmail.com>
2015-10-02 15:36:26 +0530

    Transaction: fix another Dbi typo

--- src/engine/transaction.cpp
+++ src/engine/transaction.cpp
@@ -388,7 +388,7 @@ void Transaction::renameFilePath(quint64 id, const Document& newDoc)
     Q_ASSERT(id);
 
     // Update the id -> url db
-    DocumentTimeDB docTimeDb(m_dbis.docTermsDbi, m_txn);
+    DocumentTimeDB docTimeDb(m_dbis.docTimeDbi, m_txn);
     DocumentUrlDB docUrlDb(m_dbis.idTreeDbi, m_dbis.idFilenameDbi, m_txn);
 
     docUrlDb.replace(id, newDoc.url(), [&docTimeDb](quint64 id) {
