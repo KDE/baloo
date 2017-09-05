d636fdc569ea4702d9930e409801221967714715
David Kahles <david.kahles96@gmail.com>
2017-09-06 01:00:43 +0200

    Remove unused config.h.cmake entries
    Summary:
    I'm not sure why they were introduced, but they aren't used anywhere in the
    code.
    
    Test Plan:
    - "grep -r HAVE_MALLOC_H baloo"
               - "grep -r HAVE_MALLOC_TRIM baloo"
    	   - Compiles.
    
    Reviewers: dfaure
    
    Reviewed By: dfaure
    
    Subscribers: dfaure, #frameworks
    
    Tags: #frameworks
    
    Differential Revision: https://phabricator.kde.org/D8461

--- config.h.cmake
+++
@@ -1,7 +0,0 @@
-/* This file is generated from config.h.cmake. */
-
-/* Whether malloc.h exists. */
-#cmakedefine HAVE_MALLOC_H 1
-
-/* Whether malloc_trim(3) exists. */
-#cmakedefine HAVE_MALLOC_TRIM 1
