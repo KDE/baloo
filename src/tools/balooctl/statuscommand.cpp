a7150492b691b9d0177315500abf3f71c7fe8b8c
Michael Heidelbach <ottwolt@gmail.com>
2018-01-18 10:17:49 +0100

    balooctl status: Produce parseable output
    Summary:
     - introduced -f|--format command-line option
     - implemented formatters 'simple', 'json', 'multiline' (=current output)
    
    Test Plan:
    $ balooctl status -h
    $ balooctl status /existing/path/* /not/existing/path [--format xxx]
    JSON well-formed check:
    $ balooctl status /existing/path/* /not/existing/path -f json | jq '.'
    
    Reviewers: #frameworks, vhanda, smithjd, lbeltrame
    
    Reviewed By: lbeltrame
    
    Subscribers: broulik, ngraham
    
    Tags: #frameworks
    
    Differential Revision: https://phabricator.kde.org/D9958

--- src/tools/balooctl/statuscommand.cpp
+++ src/tools/balooctl/statuscommand.cpp
@@ -45,11 +45,143 @@ QString StatusCommand::description()
     return i18n("Print the status of the Indexer");
 }
 
+void printMultiLine(Transaction& tr, IndexerConfig&  cfg, const QStringList& args) {
+    QTextStream out(stdout);
+    QTextStream err(stderr);
+    for (const QString& arg : args) {
+      QString filePath = QFileInfo(arg).absoluteFilePath();
+      quint64 id = filePathToId(QFile::encodeName(filePath));
+      if (id == 0) {
+        err << i18n("Ignoring non-existent file %1", filePath) << endl;
+        continue;
+      }
+
+
+      out << i18n("File: %1", filePath) << endl;
+      if (tr.hasDocument(id)) {
+          out << i18n("Basic Indexing: Done") << endl;
+      } else if (cfg.shouldBeIndexed(filePath)) {
+          out << i18n("Basic Indexing: Scheduled") << endl;
+          continue;
+      } else {
+          // FIXME: Add why it is not being indexed!
+          out << i18n("Basic Indexing: Disabled") << endl;
+          continue;
+      }
+
+      if (QFileInfo(arg).isDir()) {
+          continue;
+      }
+
+      if (tr.inPhaseOne(id)) {
+          out << i18n("Content Indexing: Scheduled") << endl;
+      } else if (tr.hasFailed(id)) {
+          out << i18n("Content Indexing: Failed") << endl;
+      } else {
+          out << i18n("Content Indexing: Done") << endl;
+      }
+  }
+}
+
+void printSimpleFormat(Transaction& tr, IndexerConfig&  cfg, const QStringList& args) {
+    QTextStream out(stdout);
+    QTextStream err(stderr);
+    for (const QString& arg : args) {
+      QString filePath = QFileInfo(arg).absoluteFilePath();
+      quint64 id = filePathToId(QFile::encodeName(filePath));
+      if (id == 0) {
+        err << i18n("Ignoring non-existent file %1", filePath) << endl;
+        continue;
+      }
+
+      if (QFileInfo(arg).isDir()) {
+        if (tr.hasDocument(id)) {
+            out << QLatin1String("Basic indexing done");
+        } else if (cfg.shouldBeIndexed(filePath)) {
+            out << QLatin1String("Basic indexing scheduled");
+
+        } else {
+            out << QLatin1String("Indexing disabled");
+        }
+          
+      } else if (tr.hasDocument(id)) {
+        if (tr.inPhaseOne(id)) {
+            out << QLatin1String("Content indexing scheduled");
+        } else if (tr.hasFailed(id)) {
+            out << QLatin1String("Content indexing failed");
+        } else {
+            out << QLatin1String("Content indexing done");
+        }
+
+      } else if (cfg.shouldBeIndexed(filePath)) {
+          out << QLatin1String("Basic indexing scheduled");
+      } else {
+          out << QLatin1String("Indexing disabled");
+      }
+
+      out << ":" << filePath << endl;
+  }
+}
+
+void printJSON(Transaction& tr, IndexerConfig&  cfg, const QStringList& args) {
+    
+  QJsonArray filesInfo;
+  for (const QString& arg : args) {
+      QString filePath = QFileInfo(arg).absoluteFilePath();
+      quint64 id = filePathToId(QFile::encodeName(filePath));
+      
+      if (id == 0) {
+          QTextStream err(stderr);
+          err << i18n("Ignoring non-existent file %1", filePath) << endl;
+          continue;
+      }
+      QJsonObject fileInfo;
+      fileInfo["file"] = filePath;
+      if (!tr.hasDocument(id)) {
+          fileInfo["indexing"] = "basic";
+          if (cfg.shouldBeIndexed(filePath)) {
+              fileInfo["status"] = "scheduled";
+          } else {
+              fileInfo["status"] = "disabled";
+          }
+
+      } else if (QFileInfo(arg).isDir()) {
+          fileInfo["indexing"] = "basic";
+          fileInfo["status"] =  "done";
+          
+      } else {
+          fileInfo["indexing"] = "content";
+          if (tr.inPhaseOne(id)) {
+              fileInfo["status"] = "scheduled";
+          } else if (tr.hasFailed(id)) {
+              fileInfo["status"] = "failed";
+          } else {
+              fileInfo["status"] = "done";
+          }
+      }
+      
+      filesInfo.append(fileInfo);
+  }
+  
+  QJsonDocument json;
+  json.setArray(filesInfo);
+  QTextStream out(stdout);
+  out << json.toJson(QJsonDocument::Indented);
+}
+
 int StatusCommand::exec(const QCommandLineParser& parser)
 {
     QTextStream out(stdout);
     QTextStream err(stderr);
 
+    const QStringList allowedFormats({"simple", "json", "multiline"});
+    const QString format = parser.value(QStringLiteral("format"));
+    
+    if (!allowedFormats.contains(format)) {
+        err << i18n("Output format \"%1\" is invalid", format) << endl;
+        return 1;
+    }
+    
     IndexerConfig cfg;
     if (!cfg.fileIndexingEnabled()) {
         err << i18n("Baloo is currently disabled. To enable, please run %1", QStringLiteral("balooctl enable")) << endl;
@@ -101,35 +233,12 @@ int StatusCommand::exec(const QCommandLineParser& parser)
         } else {
             out << i18n("Index does not exist yet") << endl;
         }
+    } else if (format == allowedFormats[0]){
+        printSimpleFormat(tr, cfg, args);
+    } else if (format == allowedFormats[1]){
+        printJSON(tr, cfg, args);
     } else {
-        for (const QString& arg : args) {
-            QString filePath = QFileInfo(arg).absoluteFilePath();
-            quint64 id = filePathToId(QFile::encodeName(filePath));
-
-            out << i18n("File: %1", filePath) << endl;
-            if (tr.hasDocument(id)) {
-                out << i18n("Basic Indexing: Done") << endl;
-            } else if (cfg.shouldBeIndexed(filePath)) {
-                out << i18n("Basic Indexing: Scheduled") << endl;
-                continue;
-            } else {
-                // FIXME: Add why it is not being indexed!
-                out << i18n("Basic Indexing: Disabled") << endl;
-                continue;
-            }
-
-            if (QFileInfo(arg).isDir()) {
-                continue;
-            }
-
-            if (tr.inPhaseOne(id)) {
-                out << i18n("Content Indexing: Scheduled") << endl;
-            } else if (tr.hasFailed(id)) {
-                out << i18n("Content Indexing: Failed") << endl;
-            } else {
-                out << i18n("Content Indexing: Done") << endl;
-            }
-        }
+        printMultiLine(tr, cfg, args);
     }
 
     return 0;
