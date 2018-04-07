1ea0b94497c3d0f3aea214b9b5ac9dc1f56bc50e
Michael Heidelbach <ottwolt@gmail.com>
2018-04-07 07:33:06 +0200

    advancedqueryparsertest: Add more tests
    Reviewers: #baloo, bruns
    
    Reviewed By: #baloo, bruns
    
    Subscribers: #frameworks
    
    Tags: #frameworks, #baloo
    
    Differential Revision: https://phabricator.kde.org/D11907

--- autotests/unit/lib/advancedqueryparsertest.cpp
+++ autotests/unit/lib/advancedqueryparsertest.cpp
@@ -22,6 +22,7 @@
 
 #include <QTest>
 
+Q_DECLARE_METATYPE(Baloo::Term)
 using namespace Baloo;
 
 class AdvancedQueryParserTest : public QObject
@@ -37,6 +38,10 @@ private Q_SLOTS:
     void testDateTime();
     void testOperators();
     void testBinaryOperatorMissingFirstArg();
+    void testNestedParentheses();
+    void testNestedParentheses_data();
+    void testOptimizedLogic();
+    void testOptimizedLogic_data();
 };
 
 void AdvancedQueryParserTest::testSimpleProperty()
@@ -199,6 +204,128 @@ void AdvancedQueryParserTest::testBinaryOperatorMissingFirstArg()
     QCOMPARE(term, expectedTerm);
 }
 
+void AdvancedQueryParserTest::testNestedParentheses()
+{
+    QFETCH(QString, searchInput);
+    QFETCH(QString, failmessage);
+    QFETCH(Term, expectedTerm);
+
+    AdvancedQueryParser parser;
+    const auto testTerm = parser.parse(searchInput);
+    qDebug() << "  result term" << testTerm;
+    qDebug() << "expected term" << expectedTerm;
+    if (!failmessage.isEmpty()) {
+        QEXPECT_FAIL("", qPrintable(failmessage), Continue);
+    }
+
+    QCOMPARE(testTerm, expectedTerm);
+}
+
+void AdvancedQueryParserTest::testNestedParentheses_data()
+{
+    QTest::addColumn<QString>("searchInput");
+    QTest::addColumn<Term>("expectedTerm");
+    QTest::addColumn<QString>("failmessage");
+
+    QTest::newRow("a AND b AND c AND d")
+        << QStringLiteral("a AND b AND c AND d")
+        << Term{Term::And, QList<Term>{
+            Term{QString(), QStringLiteral("a"), Term::Contains},
+            Term{QString(), QStringLiteral("b"), Term::Contains},
+            Term{QString(), QStringLiteral("c"), Term::Contains},
+            Term{QString(), QStringLiteral("d"), Term::Contains},
+        }}
+        << QString()
+        ;
+    QTest::newRow("(a AND b) AND (c OR d)")
+        << QStringLiteral("(a AND b) AND (c OR d)")
+        << Term{Term::And, QList<Term>{
+            Term{QString(), QStringLiteral("a"), Term::Contains},
+            Term{QString(), QStringLiteral("b"), Term::Contains},
+            Term{Term::Or, QList<Term>{
+                Term{QString(), QStringLiteral("c"), Term::Contains},
+                Term{QString(), QStringLiteral("d"), Term::Contains},
+            }}
+        }}
+        << QString()
+        ;
+    QTest::newRow("(a AND (b AND (c AND d)))")
+        << QStringLiteral("(a AND (b AND (c AND d)))")
+        << Term{Term::And, QList<Term>{
+            Term{QString(), QStringLiteral("a"), Term::Contains},
+            Term{QString(), QStringLiteral("b"), Term::Contains},
+            Term{QString(), QStringLiteral("c"), Term::Contains},
+            Term{QString(), QStringLiteral("d"), Term::Contains},
+        }}
+        << QStringLiteral("Fails to optimize for unknown reason, but output is semantically correct")
+        ;
+    // This test verifies that the above test is semantically correct
+    QTest::newRow("(a AND (b AND (c AND d))) semantic")
+        << QStringLiteral("(a AND (b AND (c AND d)))")
+        << Term{Term::And, QList<Term>{
+                Term{QString(), QStringLiteral("a"), Term::Contains},
+                Term{Term::And, QList<Term>{
+                    Term{QString(), QStringLiteral("b"), Term::Contains},
+                    Term{Term::And, QList<Term>{
+                        Term{QString(), QStringLiteral("c"), Term::Contains},
+                        Term{QString(), QStringLiteral("d"), Term::Contains}
+
+                    }}
+                }}
+            }}
+        << QString()
+        ;
+
+}
+
+void AdvancedQueryParserTest::testOptimizedLogic()
+{
+    QFETCH(Term, testTerm);
+    QFETCH(Term, expectedTerm);
+    qDebug() << "  result term" << testTerm;
+    qDebug() << "expected term" << expectedTerm;
+
+    QEXPECT_FAIL("", "no optimization", Continue);
+    QCOMPARE(testTerm, expectedTerm);
+}
+
+void AdvancedQueryParserTest::testOptimizedLogic_data()
+{
+    QTest::addColumn<Term>("testTerm");
+    QTest::addColumn<Term>("expectedTerm");
+
+    // a && b && c && d can be combined into one AND term with 4 subterms
+    QTest::addRow("a && b && c && d")
+        << (Term{QString(), QStringLiteral("a"), Term::Contains}
+            && Term{QString(), QStringLiteral("b"), Term::Contains}
+            && Term{QString(), QStringLiteral("c"), Term::Contains}
+            && Term{QString(), QStringLiteral("d"), Term::Contains})
+        << Term{Term::And, QList<Term>{
+            Term{QString(), QStringLiteral("a"), Term::Contains},
+            Term{QString(), QStringLiteral("b"), Term::Contains},
+            Term{QString(), QStringLiteral("c"), Term::Contains},
+            Term{QString(), QStringLiteral("d"), Term::Contains},
+        }}
+    ;
+
+    // (a AND b) AND (c OR d) can be merged as (a AND b AND (c OR D)
+    QTest::addRow("(a && b) && (c || d)")
+        << ((Term{QString(), QStringLiteral("a"), Term::Contains}
+                && Term{QString(), QStringLiteral("b"), Term::Contains})
+            && (Term{QString(), QStringLiteral("c"), Term::Contains}
+                || Term{QString(), QStringLiteral("d"), Term::Contains}
+            ))
+        << Term{Term::And, QList<Term>{
+            Term{QString(), QStringLiteral("a"), Term::Contains},
+            Term{QString(), QStringLiteral("b"), Term::Contains},
+            Term{Term::Or, QList<Term>{
+                Term{QString(), QStringLiteral("c"), Term::Contains},
+                Term{QString(), QStringLiteral("d"), Term::Contains}
+            }}
+        }}
+    ;
+
+}
 
 QTEST_MAIN(AdvancedQueryParserTest)
 
