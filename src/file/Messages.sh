b144ae060d7fa3a0f88a84c5bb7556b355338b7d
Laurent Montel <montel@kde.org>
2018-04-27 07:07:08 +0200

    Fix extract i18n from .h file

--- src/file/Messages.sh
+++ src/file/Messages.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-$XGETTEXT `find . -name "*.cpp" | grep -v "/kcm/" | grep -v "/autotests/" | grep -v "/tests/" | grep -v "/extractor/" | grep -v "/search/"` -o $podir/baloo_file5.pot
+$XGETTEXT `find . -name "*.cpp" -o -iname "*.h" | grep -v "/kcm/" | grep -v "/autotests/" | grep -v "/tests/" | grep -v "/extractor/" | grep -v "/search/"` -o $podir/baloo_file5.pot
