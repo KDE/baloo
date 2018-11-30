8a390e188be2c966f20d2c4033f379a6053f4142
Stefan Brüns <stefan.bruens@rwth-aachen.de>
2018-12-01 00:16:57 +0100

    Extend unit tests to check comparisons of multi-digit numeric properties
    Summary:
    The unit test only checked for single digit values, and failed to check
    for multi digit values. One prominent example is Rating=10, but this
    also affected any other numeric comparison.
    
    Test Plan:
    $ ctest
    $ baloosearch 'linecount>=100'
    $ baloosearch 'rating>9'
    
    Reviewers: #baloo, #frameworks, ngraham, poboiko, mgallien
    
    Reviewed By: #baloo, ngraham
    
    Subscribers: kde-frameworks-devel
    
    Tags: #frameworks, #baloo
    
    Differential Revision: https://phabricator.kde.org/D17273

--- autotests/unit/engine/postingdbtest.cpp
+++ autotests/unit/engine/postingdbtest.cpp
@@ -103,11 +103,15 @@ private Q_SLOTS:
         db.put("R1", {1, 3, 5, 7});
         db.put("R2", {1, 8});
         db.put("R3", {2, 3, 5});
+        db.put("R10", {10, 12});
+        // X20- is the internal encoding for KFileMetaData::Property::LineCount
+        db.put("X20-90", {1, 2});
+        db.put("X20-1000", {10, 11});
 
         PostingIterator* it = db.compIter("R", 2, PostingDB::GreaterEqual);
         QVERIFY(it);
 
-        QVector<quint64> result = {1, 2, 3, 5, 8};
+        QVector<quint64> result = {1, 2, 3, 5, 8, 10, 12};
         for (quint64 val : result) {
             QCOMPARE(it->next(), static_cast<quint64>(val));
             QCOMPARE(it->docId(), static_cast<quint64>(val));
@@ -116,6 +120,30 @@ private Q_SLOTS:
         it = db.compIter("R", 2, PostingDB::LessEqual);
         QVERIFY(it);
         result = {1, 3, 5, 7, 8};
+        for (quint64 val : result) {
+            QCOMPARE(it->next(), static_cast<quint64>(val));
+            QCOMPARE(it->docId(), static_cast<quint64>(val));
+        }
+
+        it = db.compIter("R", 10, PostingDB::GreaterEqual);
+        QVERIFY(it);
+        result = {10, 12};
+        for (quint64 val : result) {
+            QCOMPARE(it->next(), static_cast<quint64>(val));
+            QCOMPARE(it->docId(), static_cast<quint64>(val));
+        }
+
+        it = db.compIter("X20-", 80, PostingDB::GreaterEqual);
+        QVERIFY(it);
+        result = {1, 2, 10, 11};
+        for (quint64 val : result) {
+            QCOMPARE(it->next(), static_cast<quint64>(val));
+            QCOMPARE(it->docId(), static_cast<quint64>(val));
+        }
+
+        it = db.compIter("X20-", 100, PostingDB::GreaterEqual);
+        QVERIFY(it);
+        result = {10, 11};
         for (quint64 val : result) {
             QCOMPARE(it->next(), static_cast<quint64>(val));
             QCOMPARE(it->docId(), static_cast<quint64>(val));
