23601661ca804470da49c2d8789439aafb819530
Stefan Brüns <stefan.bruens@rwth-aachen.de>
2018-12-03 14:05:09 +0100

    Extend PositionCodec unit tests, better code coverage
    Summary:
    The test never tested position increments larger than 1, as the positions
    where divided by the docId. Multibyte (i.e. value >= 128) position
    steps where completely omitted.
    
    Use identical datasets in the functional tests and in the benchmarks.
    
    Test Plan: ctest
    
    Reviewers: #baloo, #frameworks, ngraham, astippich, poboiko, lbeltrame
    
    Reviewed By: lbeltrame
    
    Subscribers: kde-frameworks-devel
    
    Tags: #frameworks, #baloo
    
    Differential Revision: https://phabricator.kde.org/D17331

--- autotests/benchmarks/positioncodecbenchmark.cpp
+++ autotests/benchmarks/positioncodecbenchmark.cpp
@@ -31,19 +31,27 @@ class PositionCodecBenchmark : public QObject
     Q_OBJECT
 private Q_SLOTS:
     void initTestCase();
-    // data 1 - a lot of positions, small amount of PositionInfo
+    // data 1 - small number of documents, each with many positions
     void benchEncodeData1();
     void benchDecodeData1();
-    // data 2 - few positions, large amount of PositionInfo
+    // data 2 - large number of documents, few positions (10) each
     void benchEncodeData2();
     void benchDecodeData2();
+    // data 3 - small number of documents, many positions with large increment
+    void benchEncodeData3();
+    void benchDecodeData3();
 private:
     QVector<PositionInfo> m_benchmarkData1;
     QVector<PositionInfo> m_benchmarkData2;
+    QVector<PositionInfo> m_benchmarkData3;
 };
 
 void PositionCodecBenchmark::initTestCase()
 {
+    /*
+     * Same dataset as in autotests/unit/codecs/positioncodectest.cpp
+     * Correctness of encoding/decoding is checked there.
+     */
     m_benchmarkData1.clear();
     m_benchmarkData1.reserve(100);
     for(int i = 0; i < 100; ++i)
@@ -52,7 +60,7 @@ void PositionCodecBenchmark::initTestCase()
         info.docId = (i + 1) * 4711;
         info.positions.reserve(3000);
         for (int j = 0; j < 3000; j++)
-            info.positions.append(((j + 1) * 42) / info.docId);
+            info.positions.append(((j + 1) * (i + 2)));
         m_benchmarkData1.append(info);
     }
 
@@ -66,6 +74,17 @@ void PositionCodecBenchmark::initTestCase()
         m_benchmarkData2.append(info);
     }
 
+    m_benchmarkData3.clear();
+    m_benchmarkData3.reserve(200);
+    for (int i = 0; i < 200; i++) {
+        PositionInfo info;
+        info.docId = i;
+        info.positions.reserve(30000); // > 2^14 -> 3 byte VarInt32
+        for (int j = 0; j < 30000; j++)
+            info.positions.append((j + 1) * 200); // increment 200 -> 2 byte DiffVarInt32
+
+        m_benchmarkData3.append(info);
+    }
 }
 
 void PositionCodecBenchmark::benchEncodeData1()
@@ -91,6 +110,19 @@ void PositionCodecBenchmark::benchDecodeData2()
 {
     PositionCodec pc;
     const QByteArray ba = pc.encode(m_benchmarkData2);
+    QBENCHMARK { pc.decode(ba); }
+}
+
+void PositionCodecBenchmark::benchEncodeData3()
+{
+    PositionCodec pc;
+    QBENCHMARK { pc.encode(m_benchmarkData3); }
+}
+
+void PositionCodecBenchmark::benchDecodeData3()
+{
+    PositionCodec pc;
+    const QByteArray ba = pc.encode(m_benchmarkData3);
     QBENCHMARK { pc.decode(ba); }
 }
 
