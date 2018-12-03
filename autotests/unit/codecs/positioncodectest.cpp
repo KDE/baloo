b5fd732b171ebeecceb72e4f823faf56faf5b1c7
Stefan Brüns <stefan.bruens@rwth-aachen.de>
2018-12-03 14:05:09 +0100

    Extend PositionCodec unit tests, better code coverage
    Summary:
    The test never tested position increments larger than 1, as the positions
    where divided by the docId. Multibyte (i.e. value >= 128) position
    steps where completely omitted.
    
    Use identical datasets in the functional tests and in the benchmarks.
    
    Test Plan: ctest
    
    Reviewers: #baloo, #frameworks, ngraham, astippich, poboiko, lbeltrame
    
    Reviewed By: lbeltrame
    
    Subscribers: kde-frameworks-devel
    
    Tags: #frameworks, #baloo
    
    Differential Revision: https://phabricator.kde.org/D17331

--- autotests/unit/codecs/positioncodectest.cpp
+++ autotests/unit/codecs/positioncodectest.cpp
@@ -35,8 +35,12 @@ public:
 private Q_SLOTS:
     void initTestCase();
     void checkEncodeOutput();
+    void checkEncodeOutput2();
+    void checkEncodeOutput3();
 private:
     QVector<PositionInfo> m_data;
+    QVector<PositionInfo> m_data2;
+    QVector<PositionInfo> m_data3;
 };
 
 QTEST_MAIN ( PositionCodecTest )
@@ -51,8 +55,30 @@ void PositionCodecTest::initTestCase()
         info.docId = (i + 1) * 4711;
         info.positions.reserve(3000);
         for (int j = 0; j < 3000; j++)
-            info.positions.append(((j + 1) * 42) / info.docId);
+            info.positions.append((j + 1) * (i * 2));
         m_data.append(info);
+    }
+
+    m_data2.clear();
+    m_data2.reserve(5000);
+    for (int i = 0; i < 5000; i++) {
+        PositionInfo info;
+        info.docId = i;
+        info.positions = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        m_data2.append(info);
+    }
+
+    m_data3.clear();
+    m_data3.reserve(200);
+    for (int i = 0; i < 200; i++) {
+        PositionInfo info;
+        info.docId = i;
+        info.positions.reserve(30000); // > 2^14 -> 3 byte VarInt32
+        for (int j = 0; j < 30000; j++)
+            info.positions.append((j + 1) * 200); // increment 200 -> 2 byte DiffVarInt32
+
+        m_data3.append(info);
     }
 }
 
@@ -60,12 +86,36 @@ void PositionCodecTest::checkEncodeOutput()
 {
     PositionCodec pc;
     const QByteArray ba = pc.encode(m_data);
-    QCOMPARE(ba.size(), 301000);
+    QCOMPARE(ba.size(), 409000);
     const QByteArray md5 = QCryptographicHash::hash(ba, QCryptographicHash::Md5).toHex();
-    QCOMPARE(md5, QByteArray("d44a606d301937bef105411c0ee77a88"));
+    QCOMPARE(md5, QByteArray("ae49eb3279bdda36ef91d29ce3c94c2c"));
     // and now decode the whole stuff
     QVector<PositionInfo> decodedData = pc.decode(ba);
     QCOMPARE(m_data, decodedData);
+}
+
+void PositionCodecTest::checkEncodeOutput2()
+{
+    PositionCodec pc;
+    const QByteArray ba = pc.encode(m_data2);
+    QCOMPARE(ba.size(), (8 + 1 + 10) * 5000); // DocId, VarInt32 len, DiffVarInt position
+    const QByteArray md5 = QCryptographicHash::hash(ba, QCryptographicHash::Md5).toHex();
+    QCOMPARE(md5, QByteArray("2f3710246331002e2332dce560ccd783"));
+    // and now decode the whole stuff
+    QVector<PositionInfo> decodedData = pc.decode(ba);
+    QCOMPARE(m_data2, decodedData);
+}
+
+void PositionCodecTest::checkEncodeOutput3()
+{
+    PositionCodec pc;
+    const QByteArray ba = pc.encode(m_data3);
+    QCOMPARE(ba.size(), (8 + 3 + (2 * 30000)) * 200); // DocId, VarInt32 len, DiffVarInt position
+    const QByteArray md5 = QCryptographicHash::hash(ba, QCryptographicHash::Md5).toHex();
+    QCOMPARE(md5, QByteArray("79e942003c082073b4cee8e376fffdaa"));
+    // and now decode the whole stuff
+    QVector<PositionInfo> decodedData = pc.decode(ba);
+    QCOMPARE(m_data3, decodedData);
 }
 
 #include "positioncodectest.moc"
