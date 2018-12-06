508080a25687e940732fb7e4fbf1881b5dce0737
Alexander Stippich <a.stippich@gmx.net>
2018-12-06 19:22:00 +0100

    add documentation to result class
    Reviewers: #baloo, bruns
    
    Reviewed By: #baloo, bruns
    
    Subscribers: yurchor, kde-frameworks-devel
    
    Tags: #frameworks, #baloo
    
    Differential Revision: https://phabricator.kde.org/D17301

--- src/file/extractor/result.h
+++ src/file/extractor/result.h
@@ -28,6 +28,16 @@
 #include "document.h"
 #include "termgenerator.h"
 
+/**
+ * \class Result result.h
+ *
+ * \brief The result class is where all the data extracted by
+ * the KFileMetaData extractors is saved to.
+ * It implements the KFileMetaData::ExtractionResult interface.
+ * The results can be retrieved as a Baloo::Document using
+ * \c document() and stored in the database.
+ *
+ */
 class Result : public KFileMetaData::ExtractionResult
 {
 public:
@@ -38,23 +48,44 @@ public:
     void append(const QString& text) override;
     void addType(KFileMetaData::Type::Type type) override;
 
+    /**
+     * Can be used to add extraction results to an existing
+     * Baloo::Document. Has to be called before passing the
+     * Result to KFileMetaData::Extractor::extract().
+     * \param doc The Baloo::Document
+     */
     void setDocument(const Baloo::Document& doc);
 
+    /**
+     * Returns the Baloo document to which the results from the extractors
+     * are saved.
+     */
     Baloo::Document& document() {
         return m_doc;
     }
 
     /**
      * Applies the finishing touches on the document, and makes
-     * it ready to be pushed into the db
+     * it ready to be pushed into the db.
      */
     void finish();
 
 private:
+    /**
+     * The document that represents the file that is to be indexed.
+     */
     Baloo::Document m_doc;
+    /**
+     * Contains the position state of the metadata and adds it to the document.
+     */
     Baloo::TermGenerator m_termGen;
+    /**
+     * Contains the position state of plain text and adds it to the document.
+     */
     Baloo::TermGenerator m_termGenForText;
-
+    /**
+     * Contains all indexed property data from the extractors.
+     */
     QVariantMap m_map;
 };
 
