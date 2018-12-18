KDE/baloo

commit a632a72a354e3dfbb5ea43616b846fabac0afc61

Don't check if file is directory based on mime-type
Summary:
If a filename doesn't have a trailing slash, and we use `QMimeDatabase::matchTypeForFile(MatchExtension)` to guess its mimetype,
it results in `application/octet-stream` (generic type returned if `QMimeDatabase` wasn't able to guess) instead of `inode/directory`.
Because of that, indexer does try to index folders (not a big problem, but still...)

Since we've already use `QFileInfo`, use `isDir` check instead.

Test Plan: It builds. My home directory no longer pops up in `UnindexedFileIterator`

Reviewers: #baloo, #frameworks, bruns, astippich

Reviewed By: #baloo, bruns, astippich

Subscribers: bruns, kde-frameworks-devel

Tags: #frameworks, #baloo

Differential Revision: https://phabricator.kde.org/D15960

src/file/modifiedfileindexer.cpp
@@ -68,10 +68,13 @@ void ModifiedFileIndexer::run()
 
         DocumentTimeDB::TimeInfo timeInfo = tr.documentTimeInfo(fileId);
 
+        // FIXME: Using QFileInfo over here is quite expensive!
+        QFileInfo fileInfo(filePath);
+
         // A folders mtime is updated when a new file is added / removed / renamed
         // we don't really need to reindex a folder when that happens
         // In fact, we never need to reindex a folder
-        if (timeInfo.mTime && mimetype == QLatin1String("inode/directory")) {
+        if (timeInfo.mTime && fileInfo.isDir())
             continue;
         }
 

src/file/unindexedfileiterator.cpp
@@ -106,7 +106,7 @@ bool UnIndexedFileIterator::shouldIndex(const QString& filePath, const QString& 
     // A folders mtime is updated when a new file is added / removed / renamed
     // we don't really need to reindex a folder when that happens
     // In fact, we never need to reindex a folder
-    if (timeInfo.mTime && mimetype == QLatin1String("inode/directory")) {
+    if (timeInfo.mTime && fileInfo.isDir()) {
         return false;
     }
 

commit 5954363a14e86bc20eea543b25054c72cec2deb1

Fix mistakes introduced in a632a72a
Removed redundant QFileInfo definition and added forgotten bracket

src/file/modifiedfileindexer.cpp
@@ -74,12 +74,10 @@ void ModifiedFileIndexer::run()
         // A folders mtime is updated when a new file is added / removed / renamed
         // we don't really need to reindex a folder when that happens
         // In fact, we never need to reindex a folder
-        if (timeInfo.mTime && fileInfo.isDir())
+        if (timeInfo.mTime && fileInfo.isDir()) {
             continue;
         }
 
-        // FIXME: Using QFileInfo over here is quite expensive!
-        QFileInfo fileInfo(filePath);
         bool mTimeChanged = timeInfo.mTime != fileInfo.lastModified().toTime_t();
 #if QT_VERSION >= QT_VERSION_CHECK(5,10,0)
         bool cTimeChanged = timeInfo.cTime != fileInfo.metadataChangeTime().toTime_t();