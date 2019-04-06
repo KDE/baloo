KDE/baloo

commit e58804bf9eb8d507d3ef4aa8faf12d6854f5a8bc

React to config updates inside indexer
Summary:
In case a config was changed, we need to:
 1. Stop the ContentIndexer (bug 373430)
 2. Remove all scheduled indexing of files that should not be indexed
 3. Check for stale index entries. Those includes documents marked for indexing, that should be removed
(because of that we need to do it inside `scheduleIndexing` before content indexing)
 4. Finally, check for unindexed files (if new files were added)

(based on {D15939}). Also note that `IndexCleaner` should clean all files, not just those inside `includeFolders`

BUG: 373430

Test Plan:
Seems to be working. But it would be nice to test it more.

 1. Add large folder to config. `baloo_file` start indexing it.
 2. Add it to exclude list. `baloo_file` stops indexing it, and removes entries from DB.
 3. Remove it from exclude list. `baloo_file` starts indexing it again.

Reviewers: #baloo, #frameworks, bruns, ngraham

Reviewed By: #baloo, ngraham

Subscribers: kde-frameworks-devel, bruns

Tags: #frameworks, #baloo

Differential Revision: https://phabricator.kde.org/D15983

src/file/indexcleaner.cpp
@@ -77,6 +77,13 @@ void IndexCleaner::run()
         quint64 id = filePathToId(QFile::encodeName(folder));
         tr.removeRecursively(id, shouldDelete);
     }
+    const auto excludeFolders = m_config->excludeFolders();
+    for (const QString& folder : excludeFolders) {
+        quint64 id = filePathToId(QFile::encodeName(folder));
+        if (tr.hasDocument(id)) {
+            tr.removeRecursively(id);
+        }
+    }
     tr.commit();
 
     Q_EMIT done();

src/file/fileindexscheduler.cpp
@@ -124,6 +124,19 @@ void FileIndexScheduler::scheduleIndexing()
         return;
     }
 
+    // This has to be above content indexing, because there can be files that
+    // should not be indexed in the DB (i.e. if config was changed)
+    if (m_checkStaleIndexEntries) {
+        auto runnable = new IndexCleaner(m_db, m_config);
+        connect(runnable, &IndexCleaner::done, this, &FileIndexScheduler::runnerFinished);
+
+        m_threadPool.start(runnable);
+        m_checkStaleIndexEntries = false;
+        m_indexerState = StaleIndexEntriesClean;
+        Q_EMIT stateChanged(m_indexerState);
+        return;
+    }
+
     if (m_provider.size() && !m_powerMonitor.isOnBattery()) {
         m_threadPool.start(m_contentIndexer);
         m_indexerState = ContentIndexing;
@@ -142,16 +155,6 @@ void FileIndexScheduler::scheduleIndexing()
         return;
     }
 
-    if (m_checkStaleIndexEntries) {
-        auto runnable = new IndexCleaner(m_db, m_config);
-        connect(runnable, &IndexCleaner::done, this, &FileIndexScheduler::runnerFinished);
-
-        m_threadPool.start(runnable);
-        m_checkStaleIndexEntries = false;
-        m_indexerState = StaleIndexEntriesClean;
-        Q_EMIT stateChanged(m_indexerState);
-        return;
-    }
     m_indexerState = Idle;
     Q_EMIT stateChanged(m_indexerState);
 }
@@ -163,6 +166,31 @@ static void removeStartsWith(QStringList& list, const QString& dir)
             return file.startsWith(dir);
         });
     list.erase(tail, list.end());
+}
+
+static void removeShouldNotIndex(QStringList& list, FileIndexerConfig* config)
+{
+    const auto tail = std::remove_if(list.begin(), list.end(),
+        [config](const QString& file) {
+            return !config->shouldBeIndexed(file);
+        });
+    list.erase(tail, list.end());
+}
+
+void FileIndexScheduler::updateConfig()
+{
+    // Interrupt content indexer, to avoid indexing files that should
+    // not be indexed (bug 373430)
+    if (m_indexerState == ContentIndexing) {
+        m_contentIndexer->quit();
+        m_indexerState = Idle;
+    }
+    removeShouldNotIndex(m_newFiles, m_config);
+    removeShouldNotIndex(m_modifiedFiles, m_config);
+    removeShouldNotIndex(m_xattrFiles, m_config);
+    m_checkStaleIndexEntries = true;
+    m_checkUnindexedFiles = true;
+    scheduleIndexing();
 }
 
 void FileIndexScheduler::handleFileRemoved(const QString& file)

src/file/fileindexscheduler.h
@@ -85,6 +85,7 @@ public Q_SLOTS:
 
     void handleFileRemoved(const QString& file);
 
+    void updateConfig();
     void scheduleIndexing();
     void scheduleCheckUnindexedFiles();
     void scheduleCheckStaleIndexEntries();

src/file/mainhub.cpp
@@ -65,9 +65,8 @@ void MainHub::quit() const
 void MainHub::updateConfig()
 {
     m_config->forceConfigUpdate();
-    // FIXME!!
-    //m_fileIndexer.updateConfig();
     m_fileWatcher.updateIndexedFoldersWatches();
+    m_fileIndexScheduler.updateConfig();
 }
 
 void MainHub::registerBalooWatcher(const QString &service)

commit 0a154d1470f147306bf8aaab00cd52a0746d617a

[indexcleaner] Avoid removing included folders below excluded ones
Summary:
This fixes a regression introduced with commit e58804bf9eb8
("React to config updates inside indexer").

The recursion should stop when it reaches an explicitly included folder,
thus pass shouldDelete also into the iteration for excluded folders.
The current unconditional removal causes recurrent indexing of included
subtrees on each login.

Do a depth first walk, and after returning check if there are any
children left. Only delete the folder if it is empty. Otherwise, the
directory tree structure has dangling subtrees.

Test Plan:
configure an includeFolder below an excludeFolder
start balooctl monitor
restart baloo_file
files below the includeFolder should not be needed to be reindexed

Reviewers: #baloo, #frameworks, ngraham, astippich, poboiko

Reviewed By: #baloo, ngraham

Subscribers: kde-frameworks-devel

Tags: #frameworks, #baloo

Differential Revision: https://phabricator.kde.org/D20299

src/engine/writetransaction.h
@@ -59,14 +59,17 @@ public:
     void removeRecursively(quint64 parentId, Functor shouldDelete) {
         DocumentUrlDB docUrlDB(m_dbis.idTreeDbi, m_dbis.idFilenameDbi, m_txn);
 
-        if (shouldDelete(parentId)) {
-            removeRecursively(parentId);
+        if (!shouldDelete(parentId)) {
             return;
         }
 
         const QVector<quint64> children = docUrlDB.getChildren(parentId);
         for (quint64 id : children) {
             removeRecursively(id, shouldDelete);
+        }
+        // refetch
+        if (docUrlDB.getChildren(parentId).isEmpty()) {
+            removeDocument(parentId);
         }
     }
 

src/file/indexcleaner.cpp
@@ -81,7 +81,7 @@ void IndexCleaner::run()
     for (const QString& folder : excludeFolders) {
         quint64 id = filePathToId(QFile::encodeName(folder));
         if (tr.hasDocument(id)) {
-            tr.removeRecursively(id);
+            tr.removeRecursively(id, shouldDelete);
         }
     }
     tr.commit();