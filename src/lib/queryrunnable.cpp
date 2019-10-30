aa63f983bd2ce81b647f41f9f45e931e6fdab8e7
Laurent Montel <montel@kde.org>
2019-10-30 20:16:10 +0100

    Make it compile against qt5.15

--- src/lib/queryrunnable.cpp
+++ src/lib/queryrunnable.cpp
@@ -31,7 +31,11 @@ public:
     QAtomicInt m_stop;
 
     bool stopRequested() const {
+#if QT_VERSION < QT_VERSION_CHECK(5, 15, 0)
         return m_stop.load();
+#else
+        return m_stop.loadRelaxed();
+#endif
     }
 
 };
@@ -51,7 +55,11 @@ QueryRunnable::~QueryRunnable()
 
 void QueryRunnable::stop()
 {
+#if QT_VERSION < QT_VERSION_CHECK(5, 15, 0)
     d->m_stop.store(true);
+#else
+    d->m_stop.storeRelaxed(true);
+#endif
 }
 
 void QueryRunnable::run()
