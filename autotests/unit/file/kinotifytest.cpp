f4f40940e880b31648b153ba5009d13642b3b801
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2020-07-26 11:19:00 +0200

    Convert license statements to SPDX expressions

--- autotests/unit/file/kinotifytest.cpp
+++ autotests/unit/file/kinotifytest.cpp
@@ -1,23 +1,9 @@
 /*
-   Copyright (C) 2010 by Sebastian Trueg <trueg at kde.org>
-   Copyright (C) 2014 by Vishesh Handa <vhanda@kde.org>
+    SPDX-FileCopyrightText: 2010 Sebastian Trueg <trueg at kde.org>
+    SPDX-FileCopyrightText: 2014 Vishesh Handa <vhanda@kde.org>
 
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2 of
-   the License or (at your option) version 3 or any later version
-   accepted by the membership of KDE e.V. (or its successor approved
-   by the membership of KDE e.V.), which shall act as a proxy
-   defined in Section 14 of version 3 of the license.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+    SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+*/
 
 #include "kinotify.h"
 
