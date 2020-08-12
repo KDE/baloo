f9e34ba2b00ba5c983e88adf73097add31319b3c
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2020-08-12 15:40:22 +0200

    Relicense file to LGPL-2.0-or-later
    License change allowed according to relicensecheck.pl

--- src/file/priority.h
+++ src/file/priority.h
@@ -2,7 +2,7 @@
     This file is part of the KDE Project
     SPDX-FileCopyrightText: 2008 Sebastian Trueg <trueg@kde.org>
 
-    SPDX-License-Identifier: LGPL-2.0-only
+    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
 #ifndef BALOO_LINUX_PRIORITY_H_
