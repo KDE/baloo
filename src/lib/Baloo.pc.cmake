4fa870c3d4656a47c701fb60a42bbfc92f40f292
Friedrich W. H. Kossebau <kossebau@kde.org>
2021-01-29 03:12:10 +0100

    Use non-deprecated KDEInstallDirs variables also for pkg-config template
    NO_CHANGELOG

--- src/lib/Baloo.pc.cmake
+++ src/lib/Baloo.pc.cmake
@@ -1,12 +1,12 @@
 prefix=${CMAKE_INSTALL_PREFIX}
-exec_prefix=${BIN_INSTALL_DIR}
-libdir=${LIB_INSTALL_DIR}
-includedir=${KF5_INCLUDE_INSTALL_DIR}
+exec_prefix=${KDE_INSTALL_BINDIR}
+libdir=${KDE_INSTALL_LIBDIR}
+includedir=${KDE_INSTALL_INCLUDEDIR_KF5}
 
 Name: Baloo
 Description: Baloo is a file indexing and searching framework for Linux
 URL: https://www.kde.org
 Requires: Qt5Core
 Version: ${BALOO_VERSION_STRING}
-Libs: -L${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR} -lKF5Baloo
-Cflags: -I${CMAKE_INSTALL_PREFIX}/${KF5_INCLUDE_INSTALL_DIR}/Baloo
+Libs: -L${CMAKE_INSTALL_PREFIX}/${KDE_INSTALL_LIBDIR} -lKF5Baloo
+Cflags: -I${CMAKE_INSTALL_PREFIX}/${KDE_INSTALL_INCLUDEDIR_KF5}/Baloo
