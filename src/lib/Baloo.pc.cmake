fd041eaffbc8d0294c110201cd060bc27880698e
Laurent Montel <montel@kde.org>
2021-12-22 20:36:08 +0100

    Adapt build system for building against qt6

--- src/lib/Baloo.pc.cmake
+++ src/lib/Baloo.pc.cmake
@@ -1,12 +1,12 @@
 prefix=${CMAKE_INSTALL_PREFIX}
 exec_prefix=${KDE_INSTALL_BINDIR}
 libdir=${KDE_INSTALL_LIBDIR}
-includedir=${KDE_INSTALL_INCLUDEDIR_KF5}
+includedir=${KDE_INSTALL_INCLUDEDIR_KF}
 
 Name: Baloo
 Description: Baloo is a file indexing and searching framework for Linux
 URL: https://www.kde.org
-Requires: Qt5Core
+Requires: Qt${QT_MAJOR_VERSION}Core
 Version: ${BALOO_VERSION}
 Libs: -L${CMAKE_INSTALL_PREFIX}/${KDE_INSTALL_LIBDIR} -lKF5Baloo
-Cflags: -I${CMAKE_INSTALL_PREFIX}/${KDE_INSTALL_INCLUDEDIR_KF5}/Baloo
+Cflags: -I${CMAKE_INSTALL_PREFIX}/${KDE_INSTALL_INCLUDEDIR_KF}/Baloo
