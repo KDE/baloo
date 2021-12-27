66270e43d235d7980fa3d2bc6314a06ddac5728e
Allen Winter <winter@kde.org>
2021-12-27 09:30:17 -0500

    engine/idutils.h - fix compile in filePathToStat
    inside the Q_OS_WIN conditional compile the QByteArray filePath
    must be converted to a QString for use by QFileInfo.

--- src/engine/idutils.h
+++ src/engine/idutils.h
@@ -46,8 +46,9 @@ inline int filePathToStat(const QByteArray& filePath, QT_STATBUF& statBuf)
     return QT_LSTAT(filePath.constData(), &statBuf);
 #else
     const int ret = QT_STAT(filePath.constData(), &statBuf);
-    if (ret == 0 && QFileInfo(filePath).isSymLink()) {
-        return QT_STAT(QFileInfo(filePath).symLinkTarget().toUtf8().constData(), &statBuf);
+    const QString filePathStr = QString::fromUtf8(filePath);
+    if (ret == 0 && QFileInfo(filePathStr).isSymLink()) {
+        return QT_STAT(QFileInfo(filePathStr).symLinkTarget().toUtf8().constData(), &statBuf);
     } else {
         return ret;
     }
