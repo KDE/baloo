2e49baca0672f6c56d4eb406f4a9cba348a0bf14
Nicolas Fella <nicolas.fella@gmx.de>
2023-01-20 23:39:38 +0100

    Rename CMake targets/config/libraries for KF6

--- src/lib/Baloo.pc.cmake
+++ src/lib/Baloo.pc.cmake
@@ -8,5 +8,5 @@ Description: Baloo is a file indexing and searching framework for Linux
 URL: https://www.kde.org
 Requires: Qt${QT_MAJOR_VERSION}Core
 Version: ${BALOO_VERSION}
-Libs: -L${CMAKE_INSTALL_PREFIX}/${KDE_INSTALL_LIBDIR} -lKF5Baloo
+Libs: -L${CMAKE_INSTALL_PREFIX}/${KDE_INSTALL_LIBDIR} -lKF6Baloo
 Cflags: -I${CMAKE_INSTALL_PREFIX}/${KDE_INSTALL_INCLUDEDIR_KF}/Baloo
