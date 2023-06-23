70467a9e3cf9a11b24d72d669ae6638f53cd5de9
Stefan Brüns <stefan.bruens@rwth-aachen.de>
2023-06-23 02:14:49 +0200

    [BasicIndexingTest] Add options for transaction size and index dir

--- tests/engine/basicindexingtest.cpp
+++ tests/engine/basicindexingtest.cpp
@@ -5,8 +5,9 @@
     SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 */
 
-#include <QDebug>
+#include <QCommandLineParser>
 #include <QCoreApplication>
+#include <QDebug>
 #include <QTemporaryDir>
 #include <QDirIterator>
 #include <QDir>
@@ -14,7 +15,9 @@
 #include <QMimeDatabase>
 
 #include "database.h"
+#include "databasesize.h"
 #include "transaction.h"
+#include "../file/util.h"
 #include "src/file/basicindexingjob.h"
 
 using namespace Baloo;
@@ -24,39 +27,78 @@ int main(int argc, char** argv)
     QCoreApplication app(argc, argv);
 
     QTemporaryDir tempDir;
-    qDebug() << tempDir.path();
+    qDebug() << "Creating temporary DB in" << tempDir.path();
 
     Database db(tempDir.path());
     db.open(Baloo::Database::CreateDatabase);
 
     Transaction tr(db, Transaction::ReadWrite);
 
-    QMimeDatabase mimeDb;
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addOption({QStringLiteral("s"), QStringLiteral("Maximum transaction size (number of documents)"), QStringLiteral("size"), QStringLiteral("50000")});
+    parser.addPositionalArgument(QStringLiteral("dir"), QStringLiteral("Index root directory"));
+    parser.process(app);
+
+    const uint transactionSize = parser.value(QStringLiteral("s")).toUInt();
+    auto arguments = parser.positionalArguments();
+    if (arguments.size() > 1 || !transactionSize) {
+        parser.showHelp(1);
+    }
+    const QString path = [&arguments,&parser]() {
+        if (arguments.empty()) {
+            return QDir::homePath();
+        } else {
+            QFileInfo fi(arguments[0]);
+            if (fi.isDir()) {
+                return arguments[0];
+            }
+            parser.showHelp(1);
+        }
+    }();
+    qDebug() << "Indexing documents in" << path << "\nTransaction size:" << transactionSize;
+
     {
+        QMimeDatabase mimeDb;
         QElapsedTimer timer;
         timer.start();
 
-        QDirIterator it(QDir::homePath(), QDirIterator::Subdirectories);
+        QDirIterator it(path, QDir::NoDotAndDotDot | QDir::Files | QDir::Dirs, QDirIterator::Subdirectories);
         uint num = 0;
         while (it.hasNext()) {
             const QString& path = it.next();
             const QString& mimetype = mimeDb.mimeTypeForFile(path, QMimeDatabase::MatchExtension).name();
 
             BasicIndexingJob job(path, mimetype);
-            job.index();
+            if (!job.index()) {
+                continue;
+            }
 
-            tr.addDocument(job.document());
-            num++;
+            if (tr.hasDocument(job.document().id())) {
+                qDebug() << "Skip" << path;
+            } else {
+              tr.addDocument(job.document());
+              num++;
+            }
 
-            if ((num % 10000) == 0) {
-                qDebug() << num;
+            if ((num % transactionSize) == 0) {
+                tr.commit();
+                tr.reset(Transaction::ReadWrite);
+                qDebug() << num << "- Commit";
             }
         }
         tr.commit();
 
-        qDebug() << "Done" << timer.elapsed() << "msecs";
-        app.exec();
+        qDebug() << "Done -" << timer.elapsed() << "msecs," << num << "documents";
     }
+
+    {
+        Transaction tr(db, Transaction::ReadOnly);
+        const auto dbSize = tr.dbSize();
+        qDebug() << "File size (MiB):" << dbSize.actualSize / (1024.0 * 1024)
+                 << "Used:" << dbSize.expectedSize / (1024.0 * 1024);
+    }
+    printIOUsage();
 
     return 0;
 }
