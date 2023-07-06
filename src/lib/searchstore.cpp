d8e2ceb0612209ae1684b3bbacccdedbd8eb7bc8
Stefan Brüns <stefan.bruens@rwth-aachen.de>
2023-07-06 21:08:34 +0200

    [EngineQuery] Remove AND/OR operations

--- src/lib/searchstore.cpp
+++ src/lib/searchstore.cpp
@@ -113,7 +113,7 @@ EngineQuery constructEqualsQuery(const QByteArray& prefix, const QString& value)
     } else if (queries.size() == 1) {
         return queries.first();
     } else {
-        return EngineQuery(queries, EngineQuery::Phrase);
+        return EngineQuery(queries);
     }
 }
 
