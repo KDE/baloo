KDE/baloo

commit 73183acf00a2b7f10b286a1241716cb7e0785f0d

[KInotify] Fix path matching when removing watches
Summary:
As in removeWatch the path is passed in with a trailing slash, no path
is removed.

Always add a trailing slash, as otherwise `startsWith()` used for parent
checks may match erroneously (i.e. "/foo/bar2" startsWith "/foo/bar",
although it is no parent).

Depends on D29190

Test Plan: ctest -R filewatch

Reviewers: #baloo, ngraham, meven

Reviewed By: meven

Subscribers: kde-frameworks-devel

Tags: #frameworks, #baloo

Differential Revision: https://phabricator.kde.org/D29189

autotests/unit/file/filewatchtest.cpp
@@ -220,12 +220,10 @@ void FileWatchTest::testConfigChange()
     QVERIFY(createFile(d21 + "/tx2b"));
     QVERIFY(createFile(d22 + "/tx3b"));
 
-    QEXPECT_FAIL("", "Removal of excluded folders not deteced", Continue);
     QVERIFY(spyIndexNew.wait(500));
     for (const QList<QVariant>& event : qAsConst(spyIndexNew)) {
 	result.append(event.at(0).toString());
     }
-    QEXPECT_FAIL("", "Removal of excluded folders not deteced", Continue);
     QCOMPARE(result, {d21 + "/tx2b"});
 }
 

autotests/unit/file/kinotifytest.cpp
@@ -90,7 +90,7 @@ void KInotifyTest::testDeleteFolder()
 {
     // create some test files
     QTemporaryDir dir;
-    const QString d1(QStringLiteral("%1/randomJunk4").arg(dir.path()));
+    const QString d1(QStringLiteral("%1/randomJunk4/").arg(dir.path()));
     mkdir(d1);
 
     // start the inotify watcher
@@ -123,7 +123,7 @@ void KInotifyTest::testCreateFolder()
     QSignalSpy createdSpy(&kn, SIGNAL(created(QString,bool)));
 
     // create the subdir
-    const QString d1(QStringLiteral("%1/randomJunk1").arg(dir.path()));
+    const QString d1(QStringLiteral("%1/randomJunk1/").arg(dir.path()));
     mkdir(d1);
     QVERIFY(createdSpy.wait());
     QCOMPARE(createdSpy.count(), 1);
@@ -131,7 +131,7 @@ void KInotifyTest::testCreateFolder()
     QVERIFY(kn.watchingPath(d1));
 
     // lets go one level deeper
-    const QString d2 = QStringLiteral("%1/subdir1").arg(d1);
+    const QString d2 = QStringLiteral("%1subdir1/").arg(d1);
     mkdir(d2);
     QVERIFY(createdSpy.wait());
     QCOMPARE(createdSpy.count(), 1);
@@ -139,7 +139,7 @@ void KInotifyTest::testCreateFolder()
     QVERIFY(kn.watchingPath(d2));
 
     // although we are in the folder test lets try creating a file
-    const QString f1 = QStringLiteral("%1/somefile1").arg(d2);
+    const QString f1 = QStringLiteral("%1somefile1").arg(d2);
     touchFile(f1);
     QVERIFY(createdSpy.wait());
     QCOMPARE(createdSpy.count(), 1);
@@ -229,8 +229,8 @@ void KInotifyTest::testRenameFolder()
 {
     // create some test files
     QTemporaryDir dir;
-    const QString f1(QStringLiteral("%1/randomJunk1").arg(dir.path()));
-    mkdir(f1);
+    const QString d1(QStringLiteral("%1/randomJunk1/").arg(dir.path()));
+    mkdir(d1);
 
     // start the inotify watcher
     KInotify kn(nullptr /*no config*/);
@@ -239,42 +239,42 @@ void KInotifyTest::testRenameFolder()
     // listen to the desired signal
     QSignalSpy spy(&kn, SIGNAL(moved(QString,QString)));
 
-    // actually move the file
-    const QString f2(QStringLiteral("%1/randomJunk2").arg(dir.path()));
-    QFile::rename(f1, f2);
+    // actually rename the folder
+    const QString d2(QStringLiteral("%1/randomJunk2/").arg(dir.path()));
+    QFile::rename(d1, d2);
 
     // check the desired signal
     QVERIFY(spy.wait());
     QCOMPARE(spy.count(), 1);
     QList<QVariant> args = spy.takeFirst();
-    QCOMPARE(args.at(0).toString(), f1);
-    QCOMPARE(args.at(1).toString(), f2);
+    QCOMPARE(args.at(0).toString(), d1);
+    QCOMPARE(args.at(1).toString(), d2);
 
     // check the path cache
-    QVERIFY(!kn.watchingPath(f1));
-    QVERIFY(kn.watchingPath(f2));
+    QVERIFY(!kn.watchingPath(d1));
+    QVERIFY(kn.watchingPath(d2));
 
     // test a subsequent rename
-    const QString f3(QStringLiteral("%1/randomJunk3").arg(dir.path()));
-    QFile::rename(f2, f3);
+    const QString d3(QStringLiteral("%1/randomJunk3/").arg(dir.path()));
+    QFile::rename(d2, d3);
 
     // check the desired signal
     QVERIFY(spy.wait());
     QCOMPARE(spy.count(), 1);
     args = spy.takeFirst();
-    QCOMPARE(args.at(0).toString(), f2);
-    QCOMPARE(args.at(1).toString(), f3);
+    QCOMPARE(args.at(0).toString(), d2);
+    QCOMPARE(args.at(1).toString(), d3);
 
     // check the path cache
-    QVERIFY(!kn.watchingPath(f1));
-    QVERIFY(!kn.watchingPath(f2));
-    QVERIFY(kn.watchingPath(f3));
+    QVERIFY(!kn.watchingPath(d1));
+    QVERIFY(!kn.watchingPath(d2));
+    QVERIFY(kn.watchingPath(d3));
 
 
     // KInotify claims it has updated its data structures, lets see if that is true
     // by creating a file in the new folder
     // listen to the desired signal
-    const QString f4(QStringLiteral("%1/somefile").arg(f3));
+    const QString f4(QStringLiteral("%1somefile").arg(d3));
 
     QSignalSpy createdSpy(&kn, SIGNAL(created(QString,bool)));
 
@@ -292,8 +292,8 @@ void KInotifyTest::testMoveFolder()
     // create some test files
     QTemporaryDir dir1;
     QTemporaryDir dir2;
-    const QString src(QStringLiteral("%1/randomJunk1").arg(dir1.path()));
-    const QString dest(QStringLiteral("%1/randomJunk2").arg(dir2.path()));
+    const QString src(QStringLiteral("%1/randomJunk1/").arg(dir1.path()));
+    const QString dest(QStringLiteral("%1/randomJunk2/").arg(dir2.path()));
     mkdir(src);
 
     // start the inotify watcher
@@ -319,7 +319,7 @@ void KInotifyTest::testMoveFolder()
     QVERIFY(kn.watchingPath(dest));
 
     // test a subsequent move
-    const QString dest2(QStringLiteral("%1/randomJunk3").arg(dir1.path()));
+    const QString dest2(QStringLiteral("%1/randomJunk3/").arg(dir1.path()));
     QFile::rename(dest, dest2);
 
     // check the desired signal
@@ -338,7 +338,7 @@ void KInotifyTest::testMoveFolder()
     // KInotify claims it has updated its data structures, lets see if that is true
     // by creating a file in the new folder
     // listen to the desired signal
-    const QString f4(QStringLiteral("%1/somefile").arg(dest2));
+    const QString f4(QStringLiteral("%1somefile").arg(dest2));
 
     QSignalSpy createdSpy(&kn, SIGNAL(created(QString,bool)));
 

src/file/kinotify.cpp
@@ -42,12 +42,11 @@
 
 namespace
 {
-QByteArray stripTrailingSlash(const QByteArray& path)
+QByteArray normalizeTrailingSlash(QByteArray&& path)
 {
-    QByteArray p(path);
-    if (p.endsWith('/'))
-        p.chop(1);
-    return p;
+    if (!path.endsWith('/'))
+        path.append('/');
+    return path;
 }
 
 QByteArray concatPath(const QByteArray& p1, const QByteArray& p2)
@@ -122,7 +121,7 @@ public:
         // we always need the unmount event to maintain our path hash
         const int mask = newMode | newFlags | EventUnmount | FlagExclUnlink;
 
-        const QByteArray encpath = stripTrailingSlash(QFile::encodeName(path));
+        const QByteArray encpath = normalizeTrailingSlash(QFile::encodeName(path));
         int wd = inotify_add_watch(inotify(), encpath.data(), mask);
         if (wd > 0) {
 //             qCDebug(BALOO) << "Successfully added watch for" << path << watchPathHash.count();
@@ -252,7 +251,7 @@ bool KInotify::available() const
 
 bool KInotify::watchingPath(const QString& path) const
 {
-    const QByteArray p(stripTrailingSlash(QFile::encodeName(path)));
+    const QByteArray p = normalizeTrailingSlash(QFile::encodeName(path));
     return d->pathWatchHash.contains(p);
 }
 
@@ -363,6 +362,9 @@ void KInotify::slotEvent(int socket)
             const QByteArray eventName = QByteArray::fromRawData(event->name, qstrnlen(event->name, event->len));
             const QByteArray hashedPath = d->watchPathHash.value(event->wd);
             path = concatPath(hashedPath, eventName);
+            if (event->mask & IN_ISDIR) {
+                path = normalizeTrailingSlash(std::move(path));
+            }
         }
 
         Q_ASSERT(!path.isEmpty() || event->mask & EventIgnored);

commit d965d27eb85dde002333436b876318941759db50

Some indexers need non-slash trailed path
Fix regression introduced in 73183acf, prior to this commit all path
from KInotify did not have a trailing slash, adjust in consequence.

9b61371fdefbd538938f20cdc87eed03d170fa5b adjusted BasicIndexingJob but
not the other cases.

Add some missing unit test cases, although they don't cover the bug
cases, they allow to illustrate the crash conditions.

BUG: 478854

autotests/unit/file/filewatchtest.cpp
@@ -23,7 +23,9 @@ class FileWatchTest : public QObject
 private Q_SLOTS:
 
     void testFileCreation();
+    void testDirCreation();
     void testConfigChange();
+    void testFileMoved();
 
     void init()
     {
@@ -52,6 +54,10 @@ namespace {
         f1.open(QIODevice::WriteOnly);
         f1.close();
         return QFile::exists(fileUrl);
+    }
+
+    bool createDir(const QString& dirPath) {
+        return QDir().mkpath(dirPath);
     }
 
     void modifyFile(const QString& fileUrl) {
@@ -137,6 +143,72 @@ void FileWatchTest::testFileCreation()
     QCOMPARE(spyIndexXattr.takeFirst().at(0), fileUrl);
 }
 
+void FileWatchTest::testDirCreation()
+{
+    m_db->open(Baloo::Database::CreateDatabase);
+    QVERIFY(m_db->isOpen());
+
+    Test::writeIndexerConfig({m_tmpDir->path()}, {});
+    FileIndexerConfig config;
+
+    FileWatch fileWatch(m_db.get(), &config);
+    fileWatch.m_pendingFileQueue->setMaximumTimeout(0);
+    fileWatch.m_pendingFileQueue->setMinimumTimeout(0);
+    fileWatch.m_pendingFileQueue->setTrackingTime(0);
+
+    QSignalSpy spy(&fileWatch, &FileWatch::installedWatches);
+    QVERIFY(spy.isValid());
+
+    fileWatch.updateIndexedFoldersWatches();
+    QVERIFY(spy.wait());
+    QCOMPARE(spy.count(), 1);
+
+    QSignalSpy spyIndexNew(&fileWatch, &FileWatch::indexNewFile);
+    QSignalSpy spyIndexModified(&fileWatch, &FileWatch::indexModifiedFile);
+    QSignalSpy spyIndexXattr(&fileWatch, &FileWatch::indexXAttr);
+
+    QVERIFY(spyIndexNew.isValid());
+    QVERIFY(spyIndexModified.isValid());
+    QVERIFY(spyIndexXattr.isValid());
+
+    // Create a dir and see if it is indexed
+    QString dirUrl(m_tmpDir->path() + QStringLiteral("/d1"));
+    QVERIFY(createDir(dirUrl));
+    QString dirUrlTrailingSlash = QStringLiteral("%1/").arg(dirUrl);
+
+    QVERIFY(spyIndexNew.wait());
+    QCOMPARE(spyIndexNew.count(), 1);
+    QCOMPARE(spyIndexModified.count(), 0);
+    QCOMPARE(spyIndexXattr.count(), 0);
+    QCOMPARE(spyIndexNew.takeFirst().at(0), dirUrlTrailingSlash);
+
+    // Set an Xattr
+    KFileMetaData::UserMetaData umd(dirUrl);
+    if (!umd.isSupported()) {
+        qWarning() << "Xattr not supported on this filesystem:" << dirUrl;
+    } else {
+        const QString userComment(QStringLiteral("UserComment"));
+        QVERIFY(umd.setUserComment(userComment) == KFileMetaData::UserMetaData::NoError);
+
+        QVERIFY(spyIndexXattr.wait());
+        QCOMPARE(spyIndexNew.count(), 0);
+        QCOMPARE(spyIndexModified.count(), 0);
+        QCOMPARE(spyIndexXattr.count(), 1);
+        QCOMPARE(spyIndexXattr.takeFirst().at(0), dirUrlTrailingSlash);
+    }
+
+    // Change permisssions
+    QFile f(dirUrl);
+    auto permissions = f.permissions();
+    f.setPermissions(permissions & ~QFileDevice::WriteOwner);
+
+    QVERIFY(spyIndexXattr.wait());
+    QCOMPARE(spyIndexNew.count(), 0);
+    QCOMPARE(spyIndexModified.count(), 0);
+    QCOMPARE(spyIndexXattr.count(), 1);
+    QCOMPARE(spyIndexXattr.takeFirst().at(0), dirUrlTrailingSlash);
+}
+
 void FileWatchTest::testConfigChange()
 {
     m_db->open(Baloo::Database::CreateDatabase);
@@ -214,6 +286,56 @@ void FileWatchTest::testConfigChange()
 	result.append(event.at(0).toString());
     }
     QCOMPARE(result, {d21 + QStringLiteral("/tx2b")});
+}
+
+void FileWatchTest::testFileMoved()
+{
+    m_db->open(Baloo::Database::CreateDatabase);
+    QVERIFY(m_db->isOpen());
+
+    Test::writeIndexerConfig({m_tmpDir->path()}, {});
+    FileIndexerConfig config;
+
+    FileWatch fileWatch(m_db.get(), &config);
+    fileWatch.m_pendingFileQueue->setMaximumTimeout(0);
+    fileWatch.m_pendingFileQueue->setMinimumTimeout(0);
+    fileWatch.m_pendingFileQueue->setTrackingTime(0);
+
+    QSignalSpy spy(&fileWatch, &FileWatch::installedWatches);
+    QVERIFY(spy.isValid());
+
+    fileWatch.updateIndexedFoldersWatches();
+    QVERIFY(spy.wait());
+    QCOMPARE(spy.count(), 1);
+
+    QSignalSpy spyIndexNew(&fileWatch, &FileWatch::indexNewFile);
+    QSignalSpy spyIndexModified(&fileWatch, &FileWatch::indexModifiedFile);
+    QSignalSpy spyIndexFileRemoved(&fileWatch, &FileWatch::fileRemoved);
+
+    QVERIFY(spyIndexNew.isValid());
+    QVERIFY(spyIndexModified.isValid());
+    QVERIFY(spyIndexFileRemoved.isValid());
+
+    // Create a file and see if it is indexed
+    QString fileUrl(m_tmpDir->path() + QStringLiteral("/t1"));
+    QString fileDestUrl(m_tmpDir->path() + QStringLiteral("/t2"));
+    QVERIFY(createFile(fileUrl));
+
+    QVERIFY(spyIndexNew.wait());
+    QCOMPARE(spyIndexNew.count(), 1);
+    QCOMPARE(spyIndexModified.count(), 0);
+    QCOMPARE(spyIndexFileRemoved.count(), 0);
+    QCOMPARE(spyIndexNew.takeFirst().at(0), fileUrl);
+
+    // Move the file
+    QVERIFY(QFile::rename(fileUrl, fileDestUrl));
+
+    QVERIFY(spyIndexFileRemoved.wait());
+    QCOMPARE(spyIndexNew.count(), 1);
+    QCOMPARE(spyIndexNew.takeFirst().at(0), fileDestUrl);
+    QCOMPARE(spyIndexModified.count(), 0);
+    QCOMPARE(spyIndexFileRemoved.count(), 1);
+    QCOMPARE(spyIndexFileRemoved.takeFirst().at(0), fileDestUrl); // called to clean dest metadata
 }
 
 QTEST_MAIN(FileWatchTest)

src/file/metadatamover.cpp
@@ -77,10 +77,15 @@ void MetadataMover::updateMetadata(Transaction* tr, const QString& from, const Q
 {
     qCDebug(BALOO) << from << "->" << to;
     Q_ASSERT(!from.isEmpty() && !to.isEmpty());
-    Q_ASSERT(from[from.size()-1] != QLatin1Char('/'));
     Q_ASSERT(to[to.size()-1] != QLatin1Char('/'));
 
-    const QByteArray fromPath = QFile::encodeName(from);
+    // directory case
+    auto normalizedFrom = from;
+    if (normalizedFrom.endsWith(QLatin1Char('/'))) {
+        normalizedFrom.chop(1);
+    }
+
+    const QByteArray fromPath = QFile::encodeName(normalizedFrom);
     quint64 id = tr->documentId(fromPath);
     if (!id) {
         qCDebug(BALOO) << "Document not (yet) known, signaling newFile" << to;

src/file/modifiedfileindexer.cpp
@@ -37,8 +37,11 @@ void ModifiedFileIndexer::run()
 
     Transaction tr(m_db, Transaction::ReadWrite);
 
-    for (const QString& filePath : std::as_const(m_files)) {
-        Q_ASSERT(!filePath.endsWith(QLatin1Char('/')));
+    for (const QString &path : m_files) {
+        auto filePath = path;
+        if (filePath.endsWith(QLatin1Char('/'))) {
+            filePath.chop(1);
+        }
 
         QString fileName = filePath.mid(filePath.lastIndexOf(QLatin1Char('/')) + 1);
         if (!m_config->shouldFileBeIndexed(fileName)) {

src/file/newfileindexer.cpp
@@ -34,8 +34,11 @@ void NewFileIndexer::run()
 
     Transaction tr(m_db, Transaction::ReadWrite);
 
-    for (const QString& filePath : std::as_const(m_files)) {
-        Q_ASSERT(!filePath.endsWith(QLatin1Char('/')));
+    for (const QString &path : m_files) {
+        auto filePath = path;
+        if (filePath.endsWith(QLatin1Char('/'))) {
+            filePath.chop(1);
+        }
 
         QString mimetype;
         QFileInfo fileInfo(filePath);

src/file/xattrindexer.cpp
@@ -33,8 +33,11 @@ void XAttrIndexer::run()
 
     Transaction tr(m_db, Transaction::ReadWrite);
 
-    for (const QString& filePath : std::as_const(m_files)) {
-        Q_ASSERT(!filePath.endsWith(QLatin1Char('/')));
+    for (const QString &path : m_files) {
+        auto filePath = path;
+        if (filePath.endsWith(QLatin1Char('/'))) {
+            filePath.chop(1);
+        }
 
         QString fileName = filePath.mid(filePath.lastIndexOf(QLatin1Char('/')) + 1);
         if (!m_config->shouldFileBeIndexed(fileName)) {